862174d5dd91fd423071b348c6feb2ba02b848da
Jacob Lewis <mediusecho@gmail.com>
2019-02-14 15:35:54 -0800

    Registered ChatListener

--- ParticleHats/src/com/mediusecho/particlehats/managers/EventManager.java
+++ ParticleHats/src/com/mediusecho/particlehats/managers/EventManager.java
@@ -1,6 +1,7 @@
 package com.mediusecho.particlehats.managers;
 
 import com.mediusecho.particlehats.Core;
+import com.mediusecho.particlehats.listeners.ChatListener;
 import com.mediusecho.particlehats.listeners.InventoryListener;
 
 @SuppressWarnings("unused")
@@ -9,12 +10,14 @@ public class EventManager {
 	private final Core core;
 	
 	// Events
-	private InventoryListener      inventoryListener;
+	private InventoryListener inventoryListener;
+	private ChatListener      chatListener;
 	
 	public EventManager (final Core core)
 	{
 		this.core = core;
 		
-		inventoryListener      = new InventoryListener(core);
+		inventoryListener = new InventoryListener(core);
+		chatListener      = new ChatListener(core);
 	}
 }
