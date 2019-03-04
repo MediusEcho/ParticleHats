ed5c4dc9d795334d181dae69ca6ea615324e6431
Jacob Lewis <mediusecho@gmail.com>
2019-03-03 16:15:50 -0800

    Added new messages

--- ParticleHats/src/com/mediusecho/particlehats/locale/Message.java
+++ ParticleHats/src/com/mediusecho/particlehats/locale/Message.java
@@ -34,6 +34,10 @@ public enum Message {
 	COMMAND_OPEN_DESCRIPTION ("Opens a menu"),
 	COMMAND_OPEN_USAGE       ("/h open <menu name>"),
 	
+	COMMAND_ERROR_PLAYER_ONLY ("&cYou must be a player to use this command, try &7/h help"),
+	COMMAND_ERROR_ARGUMENTS   ("&cWrong number of arguments/n&c{1}"),
+	COMMAND_ERROR_MENU_EXISTS ("&7'&c{1}&7' already exists"),
+	
 	/**
 	 * Particles
 	 */
@@ -207,11 +211,88 @@ public enum Message {
 	MODE_WHEN_SWIMMING_DESCRIPTION  ("&8Only display particles while/n&8the player is swimming"),
 	
 	/**
+	 * Types
+	 */
+	TYPE_NONE_NAME                  ("&cNone"),
+	TYPE_NONE_DESCRIPTION           (""),
+	TYPE_HALO_NAME                  ("&bHalo"),
+	TYPE_HALO_DESCRIPTION           ("&8A ring of particles for the/n&8players head"),
+	TYPE_TRAIL_NAME                 ("&bTrail"),
+	TYPE_TRAIL_DESCRIPTION          ("&8Displays a small cloud of particles/n&8with random offsets"),
+	TYPE_CAPE_NAME                  ("&bCape"),
+	TYPE_CAPE_DESCRIPTION           (""),
+	TYPE_WINGS_NAME                 ("&bWings"),
+	TYPE_WINGS_DESCRIPTION          (""),
+	TYPE_VORTEX_NAME                ("&bVortex"),
+	TYPE_VORTEX_DESCRIPTION         ("&8A Swirling cone that gets/n&8larger at the bottom"),
+	TYPE_ARCH_NAME                  ("&bArch"),
+	TYPE_ARCH_DESCRIPTION           ("&8A half circle that sits/n&8above the players head"),
+	TYPE_ATOM_NAME                  ("&bAtom"),
+	TYPE_ATOM_DESCRIPTION           (""),
+	TYPE_SPHERE_NAME                ("&bSphere"),
+	TYPE_SPHERE_DESCRIPTION         (""),
+	TYPE_CRYSTAL_NAME               ("&bCrystal"),
+	TYPE_CRYSTAL_DESCRIPTION        ("&8A Plumbob, from the Sims"),
+	TYPE_HELIX_NAME                 ("&bHelix"),
+	TYPE_HELIX_DESCRIPTION          (""),
+	TYPE_INVERSE_VORTEX_NAME        ("&bInverse Vortex"),
+	TYPE_INVERSE_VORTEX_DESCRIPTION (""),
+	TYPE_HOOP_NAME                  ("&bHoop"),
+	TYPE_HOOP_DESCRIPTION           ("&82 Particles that spin around the player/n&8in a circle"),
+	TYPE_SUSANOO_NAME               ("&bSusanoo"),
+	TYPE_SUSANOO_DESCRIPTION        (""),
+	TYPE_ANGEL_WINGS_NAME           ("&bAngel Wings"),
+	TYPE_ANGEL_WINGS_DESCRIPTION    (""),
+	TYPE_CREEPER_HAT_NAME           ("&bCreeper Hat"),
+	TYPE_CREEPER_HAT_DESCRIPTION    ("&8A Creepers lovely face"),
+	TYPE_CLEAN_TRAIL_NAME           ("&bSingle Particle"),
+	TYPE_CLEAN_TRAIL_DESCRIPTION    ("&8A lonely particle, can be used/n&8to create complex designs"),
+	TYPE_TITLE_NAME                 ("&bTitle"),
+	TYPE_TITLE_DESCRIPTION          (""),
+	TYPE_CUSTOM_NAME                ("&bCustom"),
+	TYPE_CUSTOM_DESCRIPTION         (""),
+	
+	
+	/**
+	 * Meta State
+	 */
+	META_HAT_NAME_COMMAND_USAGE                 ("&6Use &f/h meta <name> &6to rename"),
+	META_HAT_NAME_DESCRIPTION                   ("Type the &eName &finto chat, or '&ccancel&f' to return"),
+	META_HAT_LABEL_USAGE                        ("&6Use &f/h meta <label> &6to rename"),
+	META_HAT_LABEL_DESCRIPTION                  ("Type the &eLabel &finto chat, or '&6cancel&f' to return"),
+	META_HAT_COMMAND_USAGE                      ("&6Use &f/h meta <command> &6to rename"),
+	META_HAT_COMMAND_DESCRIPTION                ("Type the &eCommand &7(without '/') &finto chat, or '&6cancel&f' to return"),
+	META_HAT_DESCRIPTION_USAGE                  ("&6Use &f/h meta <description> &6to rename"),
+	META_HAT_DESCRIPTION_DESCRIPTION            ("Type the &eDescription &finto chat, or '&6cancel&f' to return"),
+	META_HAT_PEMISSION_USAGE                    ("&6Use &f/h meta <permission> &6to rename"),
+	META_HAT_PEMISSION_DESCRIPTION              ("Type the &ePermission &finto chat, or '&6cancel&f' to return"),
+	META_HAT_PERMISSION_DESCRIPTION_USAGE       ("&6Use &f/h meta <description> &6to rename"),
+	META_HAT_PERMISSION_DESCRIPTION_DESCRIPTION ("Type the &eDescription &finto chat, or '&6cancel&f' to return"),
+	META_HAT_PERMISSION_MESSAGE_USAGE           ("&6Use &f/h meta <permission message> &6to rename"),
+	META_HAT_PERMISSION_MESSAGE_DESCRIPTION     ("Type the &eMessage &finto chat, or '&6cancel&f' to return"),
+	META_HAT_EQUIP_MESSAGE_USAGE                ("&6Use &f/h meta <equip message> &6to rename"),
+	META_HAT_EQUIP_MESSAGE_DESCRIPTION          ("Type the &eMessage &finto chat, or '&6cancel&f' to return"),
+	META_HAT_TAG_USAGE                          ("&6Use &f/h meta <tag> &6to set"),
+	META_HAT_TAG_DESCRIPTION                    ("Type the &eTag &finto chat, or '&6cancel&f' to return"),
+	META_MENU_TITLE_USAGE                       ("&6Use &f/h meta <title> &6to rename"),
+	META_MENU_TITLE_DESCRIPTION                 ("Type the &eTitle &finto chat, or '&6cancel&f' to return"),
+	META_NEW_MENU_NAME_USAGE                    ("&6Use &f/h meta <menu name> &6to create a new menu"),
+	META_NEW_MENU_NAME_DESCRIPTION              ("Type the &eName &finto chat, or '&6cancel&f' to return"),
+	
+	/**
 	 * Tracking
 	 */
 	TRACK_NOTHING_NAME       ("Don't Follow Movement"),
 	TRACK_HEAD_MOVEMENT_NAME ("Follow Head Movement"),
 	TRACK_BODY_MOVEMENT_NAME ("Follow Body Rotation"),
+	
+	/**
+	 * Animation
+	 */
+	ANIMATION_STATIC_NAME          ("&bStatic"),
+	ANIMATION_STATIC_DESCRIPTION   ("&8Displays all particles at once"),
+	ANIMATION_ANIMATED_NAME        ("&bAnimated"),
+	ANIMATION_ANIMATED_DESCRIPTION ("&8Iterates over a types particles/n&8displaying them as an animation"),
 	
 	/**
 	 * Display Mode
@@ -232,6 +313,8 @@ public enum Message {
 	EDITOR_MISC_PREVIOUS_PAGE ("&3Previous Page"),
 	
 	// Base Menu
+	EDITOR_EMPTY_SLOT_TITLE        ("&bEmpty Slot"),
+	EDITOR_SLOT_DESCRIPTION        ("&3Left Click to Edit/n&3Right Click for Settings"),
 	EDITOR_HAT_GENERIC_DESCRIPTION ("&7Slot &f{1}/n&7Type: &f{2=Custom}/n&7Location: &f{3}/n&7Mode: &f{4}/n&7Update: &f{5} &7tick{6=s}"),
 	EDITOR_HAT_COMMAND_DESCRIPTION (""),
 	
@@ -243,6 +326,13 @@ public enum Message {
 	EDITOR_SETTINGS_MENU_SET_PURCHASE_MENU ("&bPurchase Menu"),
 	EDITOR_SETTINGS_MENU_TOGGLE_LIVE_MENU  ("&bToggle Live Updates"),
 	EDITOR_SETTINGS_MENU_SYNC_ICONS        ("&bSync Icons"),
+	
+	EDITOR_SETTINGS_MENU_TITLE_DESCRIPTION ("&8Current Title: {1}"),
+	
+	// Resize Menu
+	EDITOR_RESIZE_MENU_TITLE               ("Resize this menu"),
+	EDITOR_RESIZE_MENU_SET_ROW_SIZE        ("&b{1} Row{2=s}"),
+	EDITOR_RESIZE_MENU_SET_ROW_DESCRIPTION ("&cResizing this menu may result/n&cin lost particle data"),
 	
 	// Main Menu
 	EDITOR_MAIN_MENU_TITLE                ("Main Menu"),
@@ -265,6 +355,7 @@ public enum Message {
 	EDITOR_MAIN_MENU_SET_SLOT             ("&bSet Slot"),
 	EDITOR_MAIN_MENU_EDIT_PARTICLES       ("&bEdit Particles"),
 	EDITOR_MAIN_MENU_NO_PARTICLES         ("&bNo Particles"),
+	EDITOR_MAIN_MENU_CUSTOM_TYPE_ERROR    ("&cUnable to find image"),
 	
 	EDITOR_MAIN_MENU_COUNT_DESCRIPTION            ("/n&8» &e{1}/n/n&3Left Click to Add 1/n&3Right Click to Subtract 1"),
 	EDITOR_MAIN_MENU_SPEED_DESCRIPTION            ("/n&8» &e{1}/n/n&3Left Click to Add 1/n&3Right Click to Subtract 1"),
@@ -274,12 +365,25 @@ public enum Message {
 	EDITOR_MAIN_MENU_MODE_DESCRIPTION             ("/n&8• {1}/n&7» &e{2}/n&8• {3}/n/n{4}/n/n&3Left Click to Cycle Down/n&3Right Click to Cycle Up"),
 	EDITOR_MAIN_MENU_UPDATE_FREQUENCY_DESCRIPTION ("/n&7» &8Updates every &e{1} &8tick{2=s}/n/n&3Left Click to Add 1/n&3Right Click to Subtract 1"),
 	EDITOR_MAIN_MENU_ICON_DESCRIPTION             ("&8Change the item that will we displayed/n&8inside this menu"),
-	EDITOR_MAIN_MENU_SOUND_DESCRIPTION            ("/n&8Sound: &7{1}/n&8Volume: &7{2}/n&8Pitch: &7{3}"),
+	EDITOR_MAIN_MENU_SOUND_DESCRIPTION            ("/n&8Sound: &7{1=&cNot Set}/n&8Volume: &7{2}/n&8Pitch: &7{3}/n/n&3Left Click to Change{4=/n&cShift Right Click to Clear}"),
 	EDITOR_MAIN_MENU_SLOT_DESCRIPTION             ("&8Change where this hat will be/n&8inside this menu"),
 	EDITOR_MAIN_MENU_ACTION_DESCRIPTION           ("/n&8Left Click Action:/n{1}/n/n&8Right Click Action:/n{2}/n/n&3Click to Change Actions"),
+	EDITOR_MAIN_MENU_META_DESCRIPTION             ("&8Edit various meta properties/n/n&3Left Click to Open/n&3Right Click for Description Shortcut"),
+	EDITOR_MAIN_MENU_TYPE_DESCRIPTION             ("/n&8Current:/n&8» {1=/n&8» }{2}/n/n&3Left Click to Change Type/n{3=&cShift Click to Change Animation}"),
+	EDITOR_MAIN_MENU_ANIMATION_DESCRIPTION        ("/n/n&8Animation:/n&8» {1}/n{2}"),
+	EDITOR_MAIN_MENU_TRACKING_METHOD_DESCRIPTION  ("{1=/n&8•}{2=/n&7»}{3=/n&8•}{4=/n/n&3Left Click to Cycle Down/n&3Right Click to Cycle Up}"),
+	
+	// Type Menu
+	EDITOR_TYPE_MENU_TITLE                   ("Select a Type {1}/{2}"),
+	EDITOR_TYPE_MENU_TYPE_PREFIX             ("&b"),
+	EDITOR_TYPE_MENU_TYPE_DESCRIPTION        ("{1=/n/n}&8Supports &3{2} &8Particle{3=s}/n/n{4=&3Click to Select}{5=&3Selected}"),
+	EDITOR_TYPE_MENU_CUSTOM_TYPE_DESCRIPTION ("{1=&3Click to Select}{2=&3Selected}"),
+	EDITOR_TYPE_MENU_INCLUDED_FILTER         ("&bIncluded Types"),
+	EDITOR_TYPE_MENU_CUSTOM_FILTER           ("&bCustom Types"),
+	EDITOR_TYPE_MENU_NO_CUSTOM_TYPES         ("&cNo Custom Types Found"),
 	
 	// Icon Menu
-	EDITOR_ICON_OVERVIEW_MENU_TITLE               ("Add / Remove Items"),
+	EDITOR_ICON_OVERVIEW_MENU_TITLE               ("Add or Remove Items"),
 	EDITOR_ICON_MENU_TITLE                        ("Select a new Item"),
 	EDITOR_ICON_MENU_INFO_TITLE                   ("&bSelect an Item"),
 	EDITOR_ICON_MENU_INFO_DESCRIPTION             ("&8Select an Item from your/n&8inventory to set as/n&8this hats icon"),
@@ -313,7 +417,7 @@ public enum Message {
 	EDITOR_ANGLE_MENU_ANGLE_Z_DESCRIPTION ("/n&8• X: &7{1}/n&8• Y: &7{2}/n&e» Z: {3}/n/n&3Left Click to Add 0.1/n&3Right Click to Subtract 0.1/n&3Shift Click to Adjust by 1/n&cMiddle Click to Reset"),
 
 	// Particle Selection Menu
-	EDITOR_PARTICLE_MENU_TITLE          ("Supported Particles {1} / {2}"),
+	EDITOR_PARTICLE_MENU_TITLE          ("Supported Particles {1}/{2}"),
 	EDITOR_PARTICLE_COLOR_FILTER_TITLE  ("Filter: Color"),
 	EDITOR_PARTICLE_DATA_FILTER_TITLE   ("Filter: Data"),
 	EDITOR_PARTICLE_RECENT_FILTER_TITLE ("Recent Particles"),
@@ -323,9 +427,9 @@ public enum Message {
 	EDITOR_PARTICLE_RECENT_FILTER       ("&3Recently Used"),
 	
 	// Sound Menu
-	EDITOR_SOUND_MENU_MISC_TITLE         ("Misc Sounds {1} / {2}"),
-	EDITOR_SOUND_MENU_BLOCK_TITLE        ("Block Sounds {1} / {2}"),
-	EDITOR_SOUND_MENU_ENTITY_TITLE       ("Entity Sounds {1} / {2}"),
+	EDITOR_SOUND_MENU_MISC_TITLE         ("Misc Sounds {1}/{2}"),
+	EDITOR_SOUND_MENU_BLOCK_TITLE        ("Block Sounds {1}/{2}"),
+	EDITOR_SOUND_MENU_ENTITY_TITLE       ("Entity Sounds {1}/{2}"),
 	EDITOR_SOUND_MENU_SET_PITCH          ("&bSet Pitch"),
 	EDITOR_SOUND_MENU_SET_VOLUME         ("&bSet Volume"),
 	EDITOR_SOUND_MENU_BLOCK_FILTER       ("&bBlock Sound Filter"),
@@ -341,7 +445,7 @@ public enum Message {
 	EDITOR_ACTION_OVERVIEW_MENU_SET_LEFT_CLICK     ("&bSet Left Click Action"),
 	EDITOR_ACTION_OVERVIEW_MENU_SET_RIGHT_CLICK    ("&bSet Right Click Action"),
 	EDITOR_ACTION_OVERVIEW_MENU_ACTION_DESCRIPTION ("/n&8Current:/n{1}/n/n&3Left Click to Change Action{2=/n&cRight Click to Change Argument}"),
-	EDITOR_ACTION_MENU_TITLE                       ("Select a {1} Action {2} / {3}"),
+	EDITOR_ACTION_MENU_TITLE                       ("Select a {1} Action {2}/{3}"),
 	EDITOR_ACTION_MENU_MISC_DESCRIPTION            ("&8» {1}"),
 	EDITOR_ACTION_MENU_MENU_DESCRIPTION            ("&8» {1}/n&8» &7{2=&cNot Set}"),
 	EDITOR_ACTION_MENU_COMMAND_DESCRIPTION         ("&8» {1}/n&8» &7{2=&cNot Set}"),
@@ -359,6 +463,35 @@ public enum Message {
 	EDITOR_DURATION_MENU_TITLE        ("Set Duration"),
 	EDITOR_DURATION_MENU_SET_DURATION ("&3Set Duration"),
 	EDITOR_DURATION_MENU_DESCRIPTION  ("/n&8» &e{1} &8second{2=s}/n/n&3Left Click to Add 1/n&3Right Click to Subtract 1/n&cShift Click to Adjust by 30"),
+	
+	// Meta Menu
+	EDITOR_META_MENU_TITLE                      ("Meta Properties"),
+	EDITOR_META_MENU_SET_NAME                   ("&bChange Display Name"),
+	EDITOR_META_MENU_SET_LABEL                  ("&bChange Label"),
+	EDITOR_META_MENU_SET_PERMISSION             ("&bChange Permission"),
+	EDITOR_META_MENU_SET_DESCRIPTION            ("&bChange Description"),
+	EDITOR_META_MENU_SET_PERMISSION_MESSAGE     ("&bChange Permission Message"),
+	EDITOR_META_MENU_SET_PERMISSION_DESCRIPTION ("&bChange Permission Description"),
+	EDITOR_META_MENU_SET_EQUIP_MESSAGE          ("&bChange Equip Message"),
+	EDITOR_META_MENU_SET_TAG                    ("&bChange Tags"),
+	
+	EDITOR_META_MENU_NAME_DESCRIPTION              ("/n&8Current:/n&8» {1}/n/n&3Left Click to Change{2=/n&cShift Right Click to Reset}"),
+	EDITOR_META_MENU_LABEL_DESCRIPTION             ("/n&8Labels allow you to use this hat/n&8in commands like: &7/h set <label>/n/n&8Current:/n&8» &7{1=&cNot Set}/n/n&3Left Click to Change{2=/n&cShift Right Click to Clear}"),
+	EDITOR_META_MENU_DESCRIPTION_DESCRIPTION       ("/n&8Current:/n{1}/n&3Left Click to Change{2=/n&cShift Right Click to Clear}"),
+	EDITOR_META_MENU_EMPTY_DESCRIPTION             ("/n&8Current:/n&8» &cNot Set/n/n&3Left Click to Change"),
+	EDITOR_META_MENU_PERMISSION_DESCRIPTION        ("/n&8Current:/n&8» &7{1}/n/n&8Usage:/n&8» &7{2}/n/n&3Click to Change"),
+	EDITOR_META_MENU_EQUIP_DESCRIPTION             ("/n&8Show the player this message when/n&8they equip this hat instead of the/n&8global equip message/n/n&8Current:/n&8» &7{1=&cNot Set}/n/n&3Left Click to Change{2=/n&cShift Right Click to Clear}"),
+	EDITOR_META_MENU_PERMISSION_DENIED_DESCRIPTION ("/n&8Current:/n&8» &7{1=&cNot Set}/n/n&3Left Click to Change{2=/n&cShift Right Click to Clear}"),
+	
+	// Description Menu
+	EDITOR_DESCRIPTION_MENU_TITLE    ("Edit Description"),
+	EDITOR_DESCRIPTION_MENU_ADD_LINE ("&bAdd a Line"),
+	EDITOR_DESCIPRION_LINE_TITLE     ("&bLine #{1}"),
+	EDITOR_DESCRIPTION_MENU_PREVIEW  ("&bPreview"),
+	EDITOR_DESCRIPTION_MENU_EMPTY    ("&cNo Description Set"),
+	
+	EDITOR_DESCRIPTION_MENU_LINE_DESCRIPTION    ("/n&8» &r{1=&cEmpty}/n/n&3Left Click to Edit/n&cShift Right Click to Delete"),
+	EDITOR_DESCRIPTION_MENU_PREVIEW_DESCRIPTION ("{1=&cEmpty}{2=/n&cShift Right Click to Clear}"),
 	
 	/**
 	 * Command Arguments
