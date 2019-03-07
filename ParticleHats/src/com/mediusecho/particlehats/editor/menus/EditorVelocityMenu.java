882b43fd90047c818f1daf1a6e50f54ab92cd365
Jacob Lewis <mediusecho@gmail.com>
2019-03-06 20:36:35 -0800

    Added VelocityMenu to edit the ItemStack particles velocity

---
+++ ParticleHats/src/com/mediusecho/particlehats/editor/menus/EditorVelocityMenu.java
@@ -0,0 +1,101 @@
+package com.mediusecho.particlehats.editor.menus;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
+
+import com.mediusecho.particlehats.Core;
+import com.mediusecho.particlehats.editor.EditorLore;
+import com.mediusecho.particlehats.editor.MenuBuilder;
+import com.mediusecho.particlehats.locale.Message;
+import com.mediusecho.particlehats.particles.Hat;
+import com.mediusecho.particlehats.particles.properties.ItemStackData;
+import com.mediusecho.particlehats.util.ItemUtil;
+import com.mediusecho.particlehats.util.MathUtil;
+
+public class EditorVelocityMenu extends EditorOffsetMenu {
+
+	private final int particleIndex;
+	
+	public EditorVelocityMenu(Core core, Player owner, MenuBuilder menuBuilder, int particleIndex, EditorGenericCallback callback) 
+	{
+		super(core, owner, menuBuilder, callback);
+		this.particleIndex = particleIndex;
+		
+		inventory = Bukkit.createInventory(null, 27, Message.EDITOR_VELOCITY_MENU_TITLE.getValue());
+		build();
+	}
+
+	public EditorClickType updateVelocity (EditorClickEvent event, Hat hat, VectorAxis axis)
+	{
+		double normalClick    = event.isLeftClick() ? 0.1f : -0.1f;
+		double shiftClick     = event.isShiftClick() ? 10 : 1;
+		double modifier       = normalClick * shiftClick;
+		boolean isMiddleClick = event.isMiddleClick();
+		
+		ItemStackData data = hat.getItemStackData(particleIndex);
+		Vector velocity = data.getVelocity();
+		switch (axis)
+		{
+		case X:
+			double vx = !isMiddleClick ? MathUtil.round(MathUtil.clamp(velocity.getX() + modifier, -20, 20), 2) : 0;
+			data.setVelocityX(vx);
+			break;
+		case Y:
+			double vy = !isMiddleClick ? MathUtil.round(MathUtil.clamp(velocity.getY() + modifier, -20, 20), 2) : 0;
+			data.setVelocityY(vy);
+			break;
+		case Z:
+			double vz = !isMiddleClick ? MathUtil.round(MathUtil.clamp(velocity.getZ() + modifier, -20, 20), 2) : 0;
+			data.setVelocityZ(vz);
+			break;
+		}
+
+		EditorLore.updateVectorDescription(getItem(14), velocity, Message.EDITOR_OFFSET_MENU_OFFSET_X_DESCRIPTION);
+		EditorLore.updateVectorDescription(getItem(15), velocity, Message.EDITOR_OFFSET_MENU_OFFSET_Y_DESCRIPTION);
+		EditorLore.updateVectorDescription(getItem(16), velocity, Message.EDITOR_OFFSET_MENU_OFFSET_Z_DESCRIPTION);
+		
+		if (event.isMiddleClick()) {
+			return EditorClickType.NEUTRAL;
+		}
+		
+		else {
+			return event.isLeftClick() ? EditorClickType.POSITIVE : EditorClickType.NEGATIVE;
+		}
+	}
+	
+	@Override
+	protected void build() 
+	{
+		Hat targetHat = menuBuilder.getTargetHat();
+		
+		// X Offset
+		ItemStack xItem = ItemUtil.createItem(Material.REPEATER, Message.EDITOR_VELOCITY_MENU_SET_VELOCITY_X);
+		EditorLore.updateVectorDescription(xItem, targetHat.getItemStackData(particleIndex).getVelocity(), Message.EDITOR_OFFSET_MENU_OFFSET_X_DESCRIPTION);
+		setButton(14, xItem, (event, slot) ->
+		{
+			return updateVelocity(event, targetHat, VectorAxis.X);
+		});
+		
+		// Y Offset
+		ItemStack yItem = ItemUtil.createItem(Material.REPEATER, Message.EDITOR_VELOCITY_MENU_SET_VELOCITY_Y);
+		EditorLore.updateVectorDescription(yItem, targetHat.getItemStackData(particleIndex).getVelocity(), Message.EDITOR_OFFSET_MENU_OFFSET_Y_DESCRIPTION);
+		setButton(15, yItem, (event, slot) ->
+		{
+			return updateVelocity(event, targetHat, VectorAxis.Y);
+		});
+		
+		// Z Offset
+		ItemStack zItem = ItemUtil.createItem(Material.REPEATER, Message.EDITOR_VELOCITY_MENU_SET_VELOCITY_Z);
+		EditorLore.updateVectorDescription(zItem, targetHat.getItemStackData(particleIndex).getVelocity(), Message.EDITOR_OFFSET_MENU_OFFSET_Z_DESCRIPTION);
+		setButton(16, zItem, (event, slot) ->
+		{
+			return updateVelocity(event, targetHat, VectorAxis.Z);
+		});
+		
+		// Back
+		setButton(10, backButton, backAction);
+	}
+}
