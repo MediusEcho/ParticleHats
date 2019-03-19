15f067390ca7a7d0cbbb4d62b131e789ae8874b7
Jacob Lewis <mediusecho@gmail.com>
2019-03-18 19:08:21 -0700

    Added clear command permission values

--- ParticleHats/src/com/mediusecho/particlehats/commands/CommandPermission.java
+++ ParticleHats/src/com/mediusecho/particlehats/commands/CommandPermission.java
@@ -5,11 +5,13 @@ public enum CommandPermission {
 	/**
 	 * Grants access to every command
 	 */
-	ALL("all"),
-	RELOAD("reload"),
-	HELP("help"),
-	EDIT("edit"),
-	CREATE("create");
+	ALL          ("all"),
+	RELOAD       ("reload"),
+	HELP         ("help"),
+	EDIT         ("edit"),
+	CREATE       ("create"),
+	CLEAR        ("clear"),
+	CLEAR_PLAYER ("clear.player");
 	
 	public static final String ROOT = "particlehats.command.";
 	public final String value;
