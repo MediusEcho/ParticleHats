46c701c5a2bae162b9127452f7de0fa17ffb2dbd
Jacob Lewis <mediusecho@gmail.com>
2019-03-18 18:22:27 -0700

    Added player state properties, Added ability to equip several hats at once, stored in a list

--- ParticleHats/src/com/mediusecho/particlehats/player/PlayerState.java
+++ ParticleHats/src/com/mediusecho/particlehats/player/PlayerState.java
@@ -1,20 +1,51 @@
 package com.mediusecho.particlehats.player;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
 import com.mediusecho.particlehats.editor.MenuBuilder;
 import com.mediusecho.particlehats.editor.MetaState;
 import com.mediusecho.particlehats.managers.SettingsManager;
+import com.mediusecho.particlehats.particles.Hat;
 import com.mediusecho.particlehats.ui.MenuState;
 
 public class PlayerState {
+	
+	private final Player owner;
 	
 	private MenuBuilder menuBuilder;
 	private MenuState menuState         = MenuState.CLOSED;
 	private MenuState previousMenuState = MenuState.CLOSED;
 	
-	private int metaDescriptionLine = 0;
-	
 	private MetaState metaState = MetaState.NONE;
 	private int metaStateTime = 15;
+	private int metaDescriptionLine = 0;
+	
+	private long lastMoveTime = 0L;
+	private long lastCombatTime = 0L;
+	
+	private AFKState afkState = AFKState.ACTIVE;
+	private PVPState pvpState = PVPState.PEACEFUL;
+	
+	private Location afkLocation;
+	
+	private List<Hat> activeHats;
+	
+	public PlayerState (final Player owner)
+	{
+		this.owner = owner;
+		
+		activeHats = new ArrayList<Hat>();
+	}
+	
+	public Player getOwner () {
+		return owner;
+	}
 
 	/**
 	 * Set this players menu builder class
@@ -75,6 +106,87 @@ public class PlayerState {
 	}
 	
 	/**
+	 * Set the time this player last moved
+	 * @param time
+	 */
+	public void setLastMoveTime (long time) {
+		lastMoveTime = time;
+	}
+	
+	/**
+	 * Get the time since this player has moved
+	 * @return
+	 */
+	public long getLastMoveTime () {
+		return lastMoveTime;
+	}
+	
+	/**
+	 * Set the time this player attacked
+	 * @param time
+	 */
+	public void setLastCombatTime (long time) {
+		lastCombatTime = time;
+	}
+	
+	/**
+	 * Get the time since this player has attacked
+	 * @return
+	 */
+	public long getLastCombatTime () {
+		return lastCombatTime;
+	}
+	
+	/**
+	 * Set the players AFKState
+	 * @param state
+	 */
+	public void setAFKState (AFKState state) {
+		afkState = state;
+	}
+	
+	/**
+	 * Get the players AFKState
+	 * @return
+	 */
+	public AFKState getAFKState () {
+		return afkState;
+	}
+	
+	/**
+	 * Set the players PVPState
+	 * @param state
+	 */
+	public void setPVPState (PVPState state) {
+		pvpState = state;
+	}
+	
+	/**
+	 * Get the players PVPState
+	 * @return
+	 */
+	public PVPState getPVPState () {
+		return pvpState;
+	}
+	
+	/**
+	 * Set the location this player is afk at
+	 * @param location
+	 */
+	public void setAFKLocation (Location location) {
+		afkLocation = location;
+	}
+	
+	/**
+	 * Get the location this player went afk at
+	 * @return
+	 */
+	@Nullable
+	public Location getAFKLocation () {
+		return afkLocation;
+	}
+	
+	/**
 	 * Set this players MetaState
 	 * @param metaState
 	 */
@@ -98,5 +210,60 @@ public class PlayerState {
 	 */
 	public int getMetaStateTime () {
 		return metaStateTime--;
+	}
+	
+	/**
+	 * Adds a hat this this players active hat list
+	 * @param hat
+	 */
+	public void addHat (Hat hat) {
+		activeHats.add(hat);
+	}
+	
+	/**
+	 * Gets all active hats
+	 * @return
+	 */
+	public List<Hat> getActiveHats () {
+		return activeHats;
+	}
+	
+	/**
+	 * Get how many hats this player has equipped
+	 * @return
+	 */
+	public int getHatCount () {
+		return activeHats.size();
+	}
+	
+	/**
+	 * Removes all active hats
+	 */
+	public void clearActiveHats () {
+		activeHats.clear();
+	}
+	
+	/**
+	 * Remove the hat at index
+	 * @param index
+	 */
+	public void removeHat (int index) {
+		activeHats.remove(index);
+	}
+	
+	public void removeHat (Hat hat) {
+		activeHats.remove(hat);
+	}
+	
+	public enum AFKState
+	{
+		ACTIVE,
+		AFK;
+	}
+	
+	public enum PVPState
+	{
+		PEACEFUL,
+		ENGAGED;
 	}
 }
