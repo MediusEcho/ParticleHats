ddf91743d169841704b273e8bc3de23188cea5fb
Jacob Lewis <mediusecho@gmail.com>
2019-03-18 18:59:00 -0700

    Added clear and clear player commands

---
+++ ParticleHats/src/com/mediusecho/particlehats/commands/subcommands/ClearCommand.java
@@ -0,0 +1,90 @@
+package com.mediusecho.particlehats.commands.subcommands;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import com.mediusecho.particlehats.Core;
+import com.mediusecho.particlehats.commands.Command;
+import com.mediusecho.particlehats.commands.CommandPermission;
+import com.mediusecho.particlehats.commands.Sender;
+import com.mediusecho.particlehats.locale.Message;
+
+public class ClearCommand extends Command {
+
+	private final ClearPlayerCommand clearPlayerCommand;
+	
+	public ClearCommand ()
+	{
+		clearPlayerCommand = new ClearPlayerCommand();
+	}
+	
+	@Override
+	public boolean execute(Core core, Sender sender, String label, ArrayList<String> args) 
+	{
+		// Self
+		if (args.size() == 0)
+		{
+			if (!sender.isPlayer())
+			{
+				sender.sendMessage(Message.COMMAND_ERROR_PLAYER_ONLY);
+				return false;
+			}
+			
+			if (!sender.hasPermission(getPermission()))
+			{
+				sender.sendMessage(Message.COMMAND_ERROR_NO_PERMISSION);
+				return false;
+			}
+			
+			core.getPlayerState(sender.getPlayerID()).clearActiveHats();
+			sender.sendMessage(Message.COMMAND_CLEAR_SUCCESS);
+			return true;
+		}
+		
+		else {
+			return clearPlayerCommand.execute(core, sender, label, args);
+		}
+	}
+	
+	@Override
+	public List<String> tabCompelete (Core core, Sender sender, String label, ArrayList<String> args)
+	{
+		if (args.size() == 1) 
+		{
+			if (sender.hasPermission(clearPlayerCommand.getPermission()))
+			{
+				List<String> players = new ArrayList<String>();
+				for (Player p : Bukkit.getOnlinePlayers()) {
+					players.add(p.getName());
+				}
+				return players;
+			}
+		}
+		return Arrays.asList("");
+	}
+
+	@Override
+	public String getName() {
+		return "clear";
+	}
+
+	@Override
+	public Message getUsage() {
+		return Message.COMMAND_CLEAR_USAGE;
+	}
+
+	@Override
+	public Message getDescription() {
+		return Message.COMMAND_CLEAR_DESCRIPTION;
+	}
+
+	@Override
+	public CommandPermission getPermission() {
+		return CommandPermission.CLEAR;
+	}
+
+}
---
+++ ParticleHats/src/com/mediusecho/particlehats/commands/subcommands/ClearPlayerCommand.java
@@ -0,0 +1,63 @@
+package com.mediusecho.particlehats.commands.subcommands;
+
+import java.util.ArrayList;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import com.mediusecho.particlehats.Core;
+import com.mediusecho.particlehats.commands.Command;
+import com.mediusecho.particlehats.commands.CommandPermission;
+import com.mediusecho.particlehats.commands.Sender;
+import com.mediusecho.particlehats.locale.Message;
+
+public class ClearPlayerCommand extends Command {
+
+	@Override
+	public boolean execute(Core core, Sender sender, String label, ArrayList<String> args) 
+	{
+		if (!sender.hasPermission(getPermission()))
+		{
+			sender.sendMessage(Message.COMMAND_ERROR_NO_PERMISSION);
+			return false;
+		}
+		
+		Player player = Bukkit.getPlayer(args.get(0));
+		if (player == null)
+		{
+			sender.sendMessage(Message.COMMAND_ERROR_UNKNOWN_PLAYER.getValue().replace("{1}", args.get(0)));
+			return false;
+		}
+		
+		if (!player.isOnline())
+		{
+			sender.sendMessage(Message.COMMAND_ERROR_OFFLINE_PLAYER.getValue().replace("{1}", player.getName()));
+			return false;
+		}
+		
+		core.getPlayerState(player.getUniqueId()).clearActiveHats();
+		sender.sendMessage(Message.COMMAND_CLEAR_PLAYER_SUCCESS.getValue().replace("{1}", player.getName()));
+		return true;
+	}
+
+	@Override
+	public String getName() {
+		return "player";
+	}
+
+	@Override
+	public Message getUsage() {
+		return Message.COMMAND_CLEAR_PLAYER_USAGE;
+	}
+
+	@Override
+	public Message getDescription() {
+		return Message.COMMAND_CLEAR_PLAYER_DESCRIPTION;
+	}
+
+	@Override
+	public CommandPermission getPermission() {
+		return CommandPermission.CLEAR_PLAYER;
+	}
+
+}
