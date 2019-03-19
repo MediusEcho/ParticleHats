7af4e14111b2d696f50ddad1dbe90fed0a15d92c
Jacob Lewis <mediusecho@gmail.com>
2019-03-18 19:04:10 -0700

    Added initial active particles menu

---
+++ ParticleHats/src/com/mediusecho/particlehats/ui/ActiveParticlesMenu.java
@@ -0,0 +1,14 @@
+package com.mediusecho.particlehats.ui;
+
+import org.bukkit.entity.Player;
+
+import com.mediusecho.particlehats.Core;
+
+public class ActiveParticlesMenu extends Menu {
+
+	public ActiveParticlesMenu(Core core, Player owner) 
+	{
+		super(core, owner);
+	}
+
+}
