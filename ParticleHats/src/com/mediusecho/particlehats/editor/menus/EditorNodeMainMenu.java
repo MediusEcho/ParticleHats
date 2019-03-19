a135e7bd7f63e4587bf0397cc9f16d7ec1db25d1
Jacob Lewis <mediusecho@gmail.com>
2019-03-18 18:14:19 -0700

    Added node menu and node overview menu

---
+++ ParticleHats/src/com/mediusecho/particlehats/editor/menus/EditorNodeMainMenu.java
@@ -0,0 +1,32 @@
+package com.mediusecho.particlehats.editor.menus;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import com.mediusecho.particlehats.Core;
+import com.mediusecho.particlehats.editor.MenuBuilder;
+import com.mediusecho.particlehats.locale.Message;
+
+public class EditorNodeMainMenu extends EditorMainMenu {
+
+	public EditorNodeMainMenu(Core core, Player owner, MenuBuilder menuBuilder) 
+	{
+		super(core, owner, menuBuilder);
+		
+		particleItemSlot = 13;
+		trackingItemSlot = 19;
+		countItemSlot    = 15;
+		equipItemSlot    = 42;
+		
+		inventory = Bukkit.createInventory(null, 45, Message.EDITOR_MAIN_MENU_TITLE.getValue());
+		build();
+	}
+
+	@Override
+	protected void build() 
+	{
+		buildSection();
+		
+		setButton(38, backButton, backAction);
+	}
+}
---
+++ ParticleHats/src/com/mediusecho/particlehats/editor/menus/EditorNodeOverviewMenu.java
@@ -0,0 +1,145 @@
+package com.mediusecho.particlehats.editor.menus;
+
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import com.mediusecho.particlehats.Core;
+import com.mediusecho.particlehats.editor.EditorListMenu;
+import com.mediusecho.particlehats.editor.MenuBuilder;
+import com.mediusecho.particlehats.locale.Message;
+import com.mediusecho.particlehats.particles.Hat;
+import com.mediusecho.particlehats.util.ItemUtil;
+import com.mediusecho.particlehats.util.StringUtil;
+
+// TODO: Add Delete option
+
+public class EditorNodeOverviewMenu extends EditorListMenu {
+
+	private final Hat targetHat;
+	private final String nodeTitle = Message.EDITOR_NODE_OVERVIEW_NODE_TITLE.getValue();
+	
+	public EditorNodeOverviewMenu(Core core, Player owner, MenuBuilder menuBuilder) 
+	{
+		super(core, owner, menuBuilder);
+		targetHat = menuBuilder.getBaseHat();
+		
+		editAction = (event, slot) ->
+		{
+			if (event.isLeftClick())
+			{
+				int index = getClampedIndex(slot, 10, 2);
+				Core.debug("looking for node at index: " + index);
+				Core.debug("node size: " + targetHat.getNodes().size());
+				
+				Hat node = targetHat.getNode(index);
+				if (node != null)
+				{
+					menuBuilder.setTargetNodeHat(node);
+					
+					EditorNodeMainMenu editorNodeMainMenu = new EditorNodeMainMenu(core, owner, menuBuilder);
+					menuBuilder.addMenu(editorNodeMainMenu);
+					editorNodeMainMenu.open();
+				}
+			}
+			
+			else if (event.isShiftRightClick()) {
+				onDelete(slot);
+			}
+			return EditorClickType.NEUTRAL;
+		};
+		
+		inventory = Bukkit.createInventory(null, 54, Message.EDITOR_NODE_OVERVIEW_MENU_TITLE.getValue());
+		build();
+	}
+	
+	// TODO: Add node description
+	
+	private void onAdd (int slot)
+	{
+		List<Hat> nodes = targetHat.getNodes();
+		int size = nodes.size();		
+		
+		if (size <= 27)
+		{
+			int index = size > 0 ? nodes.get(size - 1).getIndex() + 1 : 0;
+			Hat node = new Hat();
+			
+			node.setIndex(index);
+			nodes.add(node);
+			
+			String title = nodeTitle.replace("{1}", Integer.toString(size + 1));
+			ItemStack item = ItemUtil.createItem(Material.LEATHER_HELMET, title, StringUtil.parseDescription(Message.EDITOR_NODE_OVERVIEW_MENU_NODE_DESCRIPTION.getValue()));
+		
+			setItem(getNormalIndex(size, 10, 2), item);
+		}
+		
+		if (isEmpty)
+		{
+			isEmpty = false;
+			removeEmptyItem();
+		}
+	}
+	
+	@Override
+	protected void onDelete (int slot)
+	{
+		super.onDelete(slot);
+		
+		int index = getClampedIndex(slot, 10, 2);
+		targetHat.getNodes().remove(index);
+		
+		for (int i = index; i <= 27; i++)
+		{
+			EditorAction action = getAction(i);
+			if (action == addAction) {
+				break;
+			}
+			
+			ItemStack item = getItem(getNormalIndex(i, 10, 2));
+			ItemUtil.setItemName(item, nodeTitle.replace("{1}", Integer.toString(i + 1)));
+		}
+		
+		isEmpty = targetHat.getNodes().size() == 0;
+		if (isEmpty) {
+			insertEmptyItem();
+		}
+	}
+
+	@Override
+	public void build ()
+	{
+		super.build();
+		
+		setButton(49, backButton, (event, slot) ->
+		{
+			menuBuilder.setTargetNodeHat(null);
+			menuBuilder.goBack();
+			return EditorClickType.NEUTRAL;
+		});
+		
+		ItemStack addItem = ItemUtil.createItem(Material.TURTLE_HELMET, Message.EDITOR_NODE_OVERVIEW_MENU_ADD_NODE);
+		setButton(52, addItem, (event, slot) ->
+		{
+			onAdd(slot);
+			return EditorClickType.NEUTRAL;
+		});
+		
+		List<Hat> hatNodes = targetHat.getNodes();
+		for (int i = 0; i < hatNodes.size(); i++)
+		{			
+			String title = nodeTitle.replace("{1}", Integer.toString(i + 1));
+			ItemStack item = ItemUtil.createItem(Material.LEATHER_HELMET, title, StringUtil.parseDescription(Message.EDITOR_NODE_OVERVIEW_MENU_NODE_DESCRIPTION.getValue()));
+		
+			setItem(getNormalIndex(i, 10, 2), item);
+		}
+		
+		isEmpty = hatNodes.size() == 0;
+		if (isEmpty) {
+			insertEmptyItem();
+		}
+	}
+}
