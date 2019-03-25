b6d9ef23c0d48bba47a1c70eedc67e914cde3abc
Jacob Lewis <mediusecho@gmail.com>
2019-03-24 19:23:16 -0700

    Added method #copyFile to allow files bundled with the .jar to be copied into the plugins data folder

--- ParticleHats/src/com/mediusecho/particlehats/util/ResourceUtil.java
+++ ParticleHats/src/com/mediusecho/particlehats/util/ResourceUtil.java
@@ -1,8 +1,11 @@
 package com.mediusecho.particlehats.util;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 import javax.imageio.ImageIO;
 
@@ -25,5 +28,25 @@ public class ResourceUtil {
 		catch (IOException e) {
 			return null;
 		}
+	}
+	
+	/**
+	 * Copies the file provided
+	 * @param File to copy
+	 * @param File to copy to
+	 * @throws IOException 
+	 */
+	public static void copyFile (InputStream in, File file) throws IOException  
+	{	
+		OutputStream out = new FileOutputStream(file);
+		byte[] buf = new byte[1024];
+		
+		int len;
+		while ((len = in.read(buf)) > 0) {
+			out.write(buf, 0, len);
+		}
+		
+		out.close();
+		in.close();
 	}
 }
