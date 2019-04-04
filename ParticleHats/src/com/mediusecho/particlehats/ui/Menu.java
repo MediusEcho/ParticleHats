f970f99b56845a393a855dbc3e7a98b1c4a25498
Jacob Lewis <mediusecho@gmail.com>
2019-04-03 19:42:51 -0700

    Added methods to get the menu owner and id, as well as the name of the menu.

--- ParticleHats/src/com/mediusecho/particlehats/ui/Menu.java
+++ ParticleHats/src/com/mediusecho/particlehats/ui/Menu.java
@@ -2,21 +2,28 @@ package com.mediusecho.particlehats.ui;
 
 import java.util.UUID;
 
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
 
 import com.mediusecho.particlehats.Core;
+import com.mediusecho.particlehats.managers.SettingsManager;
+import com.mediusecho.particlehats.util.MathUtil;
 
 public abstract class Menu {
 
-	private Player owner;
-	private UUID ownerID;
+	protected final Core core;
 	
-	private MenuInventory inventory;
+	protected Player owner;
+	protected UUID ownerID;
+	
+	protected MenuInventory inventory;
 	
 	public Menu (Core core, final Player owner)
 	{
+		this.core = core;
 		this.owner = owner;
-		this.ownerID = ownerID;
+		this.ownerID = owner.getUniqueId();
 	}
 	
 	public Menu (Core core, final Player owner, final MenuInventory inventory)
@@ -25,6 +32,33 @@ public abstract class Menu {
 		this.inventory = inventory;
 	}
 	
+	/**
+	 * Get the owner of this menu
+	 * @return
+	 */
+	public Player getOwner () {
+		return owner;
+	}
+	
+	/**
+	 * Get the owners UUID
+	 * @return
+	 */
+	public UUID getOwnerID () {
+		return ownerID;
+	}
+	
+	/**
+	 * Get this menus name
+	 * @return
+	 */
+	public String getName () {
+		return inventory.getName();
+	}
+	
+	/**
+	 * Opens this menu for the owning player
+	 */
 	public void open () {
 		inventory.open(owner);
 	}
@@ -38,108 +72,54 @@ public abstract class Menu {
 	}
 	
 	/**
+	 * Plays a sound for the menu owner<br>
+	 * Sound is defined in config.yml
+	 */
+	public void playSound ()
+	{
+		if (SettingsManager.MENU_SOUND_ENABLED.getBoolean())
+		{
+			Sound sound = SettingsManager.MENU_SOUND_ID.getSound();
+			if (sound != null)
+			{
+				float volume = (float) SettingsManager.MENU_SOUND_VOLUME.getDouble();
+				float pitch = (float) SettingsManager.MENU_SOUND_PITCH.getDouble();
+				float p = (float) MathUtil.clamp(pitch, 0, 2);
+				
+				owner.playSound(owner.getLocation(), sound, volume, p);
+			}
+		}
+	}
+	
+	/**
 	 * Lets this menu update dynamically
 	 */
-	protected void onTick () {}
+	public abstract void onTick (int ticks);
+
+	/**
+	 * Handle click events inside this menu
+	 * @param event
+	 */
+	public abstract void onClick (InventoryClickEvent event);
 	
-//	private Inventory inventory;
+	/**
+	 * Creates a copy of this Menu object
+	 */
+	public Menu clone () {
+		return new StaticMenu(core, owner, inventory.clone());
+	}
 	
-//	public Menu (Core core, final Player owner)
-//	{
-//		this.owner = owner;
-//		this.ownerID = owner.getUniqueId();
-//	}
+	@Override
+	public int hashCode () {
+		return getName().hashCode();
+	}
 	
-//	public Menu (Player owner, final String title, final int rows)
-//	{
-//		this.owner = owner;
-//		this.ownerID = owner.getUniqueId();
-//		
-//		inventory = Bukkit.createInventory(null, rows * 9, ChatColor.translateAlternateColorCodes('&', title));
-//	}
-//	
-//	/**
-//	 * Opens this menu for the owner
-//	 */
-//	public void open ()
-//	{
-//		if (owner != null && inventory != null) {
-//			owner.openInventory(inventory);
-//		} else {
-//			Core.log("Unable to open menu");
-//		}
-//	}
-//	
-//	public void setItem (int slot, ItemStack item) {
-//		inventory.setItem(slot, item);
-//	}
-	
-//	public void open (String menuName)
-//	{
-//		DatabaseType databaseType = Core.instance.getDatabaseType();
-//		if (databaseType.equals(DatabaseType.MYSQL))
-//		{
-//			MySQLDatabase database = (MySQLDatabase) Core.instance.getDatabase();
-//			database.connect((connection) ->
-//			{
-//				// Create our menu
-//				String menuQuery = "SELECT title, size FROM menus WHERE name = ?";
-//				try (PreparedStatement menuStatement = connection.prepareStatement(menuQuery))
-//				{
-//					menuStatement.setString(1, menuName);
-//					ResultSet menuSet = menuStatement.executeQuery();
-//					
-//					while (menuSet.next())
-//					{
-//						final String title = ChatColor.translateAlternateColorCodes('&', menuSet.getString("title"));
-//						final int size = menuSet.getInt("size");
-//						inventory = Bukkit.createInventory(null, size * 9, title);
-//					}
-//					
-//					// Load our items
-//					String itemQuery = "SELECT slot, id, name FROM " + menuName + "_items";
-//					try (PreparedStatement itemStatement = connection.prepareStatement(itemQuery))
-//					{
-//						ResultSet itemSet = itemStatement.executeQuery();
-//						while (itemSet.next())
-//						{
-//							final int slot = itemSet.getInt("slot");
-//							final Material mat = Material.valueOf(itemSet.getString("id"));
-//							final String title = ChatColor.translateAlternateColorCodes('&', itemSet.getString("name"));
-//							
-//							ItemStack item = new ItemStack(mat);
-//							ItemMeta meta = item.getItemMeta();
-//							
-//							if (meta != null)
-//							{
-//								meta.setDisplayName(title);
-//								
-//								// Load our descriptions
-//								String descriptionQuery = "SELECT value FROM " + menuName + "_desc WHERE slot = ? ORDER BY line ASC";
-//								try (PreparedStatement descStatement = connection.prepareStatement(descriptionQuery))
-//								{
-//									descStatement.setInt(1, slot);
-//									
-//									List<String> description = new ArrayList<String>();
-//									ResultSet descResult = descStatement.executeQuery();
-//									
-//									while (descResult.next())
-//									{
-//										String entry = ChatColor.translateAlternateColorCodes('&', descResult.getString("value"));	
-//										description.add(entry);
-//									}
-//
-//									meta.setLore(description);
-//								}
-//							}
-//							
-//							item.setItemMeta(meta);
-//							setItem(slot, item);
-//						}
-//					}
-//				}
-//			});
-//		}
-//		open();
-//	}
+	@Override
+	public boolean equals (Object o)
+	{
+		if (o instanceof String) {
+			return getName().equals((String)o);
+		}
+		return false;
+	}
 }
