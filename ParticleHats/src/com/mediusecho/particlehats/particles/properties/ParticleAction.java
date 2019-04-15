25d24f1d04304019d9dc12ad2c28f7a87502ffbc
Jacob Lewis <mediusecho@gmail.com>
2019-04-14 18:53:49 -0700

    Implemented partial functionality

--- ParticleHats/src/com/mediusecho/particlehats/particles/properties/ParticleAction.java
+++ ParticleHats/src/com/mediusecho/particlehats/particles/properties/ParticleAction.java
@@ -1,11 +1,30 @@
 package com.mediusecho.particlehats.particles.properties;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
+import com.mediusecho.particlehats.Core;
+import com.mediusecho.particlehats.events.EquipEvent;
+import com.mediusecho.particlehats.hooks.CurrencyHook;
 import com.mediusecho.particlehats.locale.Message;
+import com.mediusecho.particlehats.managers.MenuManager;
+import com.mediusecho.particlehats.managers.SettingsManager;
+import com.mediusecho.particlehats.particles.Hat;
+import com.mediusecho.particlehats.permission.Permission;
+import com.mediusecho.particlehats.player.PlayerState;
+import com.mediusecho.particlehats.ui.ActiveParticlesMenu;
+import com.mediusecho.particlehats.ui.Menu;
+import com.mediusecho.particlehats.ui.MenuInventory;
+import com.mediusecho.particlehats.ui.MenuState;
+import com.mediusecho.particlehats.ui.StaticMenu;
+import com.mediusecho.particlehats.util.ItemUtil;
 
 public enum ParticleAction {
 
@@ -24,6 +43,8 @@ public enum ParticleAction {
 	MIMIC                (12, false),
 	DEMO                 (13, true),
 	ACTIVE_PARTICLES     (14, false);
+	
+	private final Core core = Core.instance;
 	
 	private final int id;
 	private final boolean hasData;
@@ -91,6 +112,276 @@ public enum ParticleAction {
 			return Message.valueOf(key).getValue();
 		} catch (IllegalArgumentException e) {
 			return "";
+		}
+	}
+	
+	/**
+	 * Perform this action
+	 */
+	@SuppressWarnings("incomplete-switch")
+	public void onClick (Player player, Hat hat, int slot, MenuInventory inventory, String argument)
+	{
+		switch (this)
+		{
+			case EQUIP:
+			{
+				EquipEvent event = new EquipEvent(player, hat);
+				Bukkit.getPluginManager().callEvent(event);
+				
+				if (!event.isCancelled()) 
+				{
+					PlayerState playerState = core.getPlayerState(player.getUniqueId());
+					
+					List<Hat> equippedHats = playerState.getActiveHats();
+					if (equippedHats.contains(hat))
+					{
+						playerState.removeHat(hat);
+						ItemStack item = inventory.getItem(slot);
+						
+						ItemUtil.stripHighlight(item);
+						return;
+					}
+					
+					if (!playerState.canEquip())
+					{
+						player.sendMessage(Message.HAT_EQUIPPED_OVERFLOW.getValue());
+						return;
+					}
+					
+					boolean canClose = SettingsManager.CLOSE_MENU_ON_EQUIP.getBoolean();	
+					String worldName = player.getWorld().getName().toLowerCase();
+					
+					// Disabled worlds
+					List<String> disabledWorlds = SettingsManager.DISABLED_WORLDS.getList();
+					if (disabledWorlds.contains(worldName))
+					{
+						player.sendMessage(Message.WORLD_DISABLED.getValue());
+	
+						if (canClose) {
+							player.closeInventory();
+						}
+						
+						return;
+					}
+					
+					// World Permission
+					if (SettingsManager.CHECK_WORLD_PERMISSION.getBoolean())
+					{
+						if (!player.hasPermission(Permission.WORLD_ALL.getPermission()) && !player.hasPermission(Permission.WORLD.append(worldName)))
+						{
+							player.sendMessage(Message.WORLD_NO_PERMISSION.getValue());
+							
+							if (canClose) {
+								player.closeInventory();
+							}
+							
+							return;
+						}
+					}
+					
+					// Check to see if we have already purchased this hat
+					if (playerState.hasPurchased(hat))
+					{
+						core.getParticleManager().equipHat(player.getUniqueId(), hat);
+						return;
+					}
+					
+					boolean canUsePermission = SettingsManager.FLAG_PERMISSION.getBoolean();
+					boolean canUseCurrency = SettingsManager.FLAG_VAULT.getBoolean() || SettingsManager.FLAG_PLAYERPOINTS.getBoolean();
+					boolean canUseExp = SettingsManager.FLAG_EXPERIENCE.getBoolean();
+					
+					if (canUsePermission)
+					{
+						if (!player.hasPermission(Permission.PARTICLE_ALL.getPermission()) && hat.isLocked())
+						{
+							// Only show the permission denied message if vault and exp are also disabled
+							if (!canUseCurrency && !canUseExp)
+							{
+								String deniedMessage = hat.getPermissionDeniedDisplayMessage();
+								
+								if (!deniedMessage.equals("")) {
+									player.sendMessage(deniedMessage);
+								} else {
+									player.sendMessage(Message.HAT_NO_PERMISSION.getValue());
+								}
+								
+								if (canClose) {
+									player.closeInventory();
+								}
+								
+								return;
+							}
+						}
+						
+						// We have permission
+						else
+						{
+							core.getParticleManager().equipHat(player.getUniqueId(), hat);
+							
+							if (canClose) {
+								player.closeInventory();
+							}
+							
+							return;
+						}
+					}
+					
+					// Try to purchase this hat
+					double playerBalance = -1;
+					
+					if (canUseCurrency) 
+					{
+						CurrencyHook currencyHook = core.getHookManager().getCurrencyHook();
+						if (currencyHook != null && currencyHook.isEnabled()) {
+							playerBalance = currencyHook.getBalance(player);
+						}
+					}
+					
+					else if (canUseExp) {
+						playerBalance = player.getLevel();
+					}
+					
+					if (playerBalance > -1)
+					{
+						if (playerBalance < hat.getPrice())
+						{
+							String currency = SettingsManager.CURRENCY.getString();
+							player.sendMessage(Message.INSUFFICIENT_FUNDS.getValue().replace("{1}", currency));
+							
+							if (canClose) {
+								player.closeInventory();
+							}
+							
+							return;
+						}
+						
+						// TODO: Add Purchase Menu
+					}
+					
+					else
+					{
+						if (!canUsePermission && !canUseCurrency && !canUseExp) {
+							core.getParticleManager().equipHat(player.getUniqueId(), hat);
+						}
+						
+						if (canClose) {
+							player.closeInventory();
+						}
+					}
+				}
+				break;
+			}
+			
+			case TOGGLE:
+			{
+				PlayerState playerState = core.getPlayerState(player.getUniqueId());
+				List<Hat> hats = playerState.getActiveHats();
+				
+				// Toggles all active hats based on the first hats toggle state
+				// Players can toggle individual hats through the hat manager
+				
+				if (hats.size() > 0)
+				{
+					boolean initialToggle = hats.get(0).isVanished();
+					for (Hat h : hats) {
+						h.setVanished(!initialToggle);
+					}
+				}
+				break;
+			}
+			
+			case CLOSE:
+			{
+				player.closeInventory();
+				break;
+			}
+			
+			case OVERRIDE:
+			{
+				core.getParticleManager().equipHat(player.getUniqueId(), hat);
+				break;
+			}
+			
+			case CLEAR:
+			{
+				core.getPlayerState(player.getUniqueId()).clearActiveHats();
+				player.sendMessage(Message.COMMAND_CLEAR_SUCCESS.getValue());
+				break;
+			}
+			
+			case COMMAND:
+			{
+				if (!argument.equals("")) 
+				{
+					player.performCommand(argument);
+					player.closeInventory();
+				}
+				break;
+			}
+			
+			case OPEN_MENU_PERMISSION:
+			case OPEN_MENU:
+			{				
+				if (this == OPEN_MENU_PERMISSION && !player.hasPermission(hat.getPermission())) {
+					break;
+				}
+				
+				MenuManager menuManager = core.getMenuManager();
+				UUID playerID = player.getUniqueId();
+				Menu menu = menuManager.getMenu(playerID, argument);
+				
+				if (menu == null)
+				{
+					MenuInventory inv = core.getDatabase().loadInventory(argument, player);
+					if (inv == null)
+					{
+						player.sendMessage(Message.COMMAND_ERROR_UNKNOWN_MENU.getValue().replace("{1}", argument));
+						break;
+					}
+					
+					menu = new StaticMenu(core, player, inv);
+				}
+				
+				core.getMenuManager().openMenu(menu, true);
+				break;
+			}
+			
+			case PURCHASE_CONFIRM:
+			{
+				// TODO: Finish purchase confirm action
+				break;
+			}
+			
+			case PURCHASE_DENY:
+			{
+				// TODO: Finish purchase deny action
+				break;
+			}
+			
+			case DEMO:
+			{
+				EquipEvent event = new EquipEvent(player, hat);
+				Bukkit.getPluginManager().callEvent(event);
+				
+				if (!event.isCancelled()) 
+				{
+					hat.setPermanent(false);
+					core.getParticleManager().equipHat(player.getUniqueId(), hat);
+				}
+				break;
+			}
+			
+			case ACTIVE_PARTICLES:
+			{
+				ActiveParticlesMenu activeParticlesMenu = new ActiveParticlesMenu(core, player, true);
+				PlayerState playerState = core.getPlayerState(player.getUniqueId());
+				
+				playerState.setActiveParticlesMenu(activeParticlesMenu);
+				playerState.setMenuState(MenuState.ACTIVE_PARTICLES);
+				
+				activeParticlesMenu.open();
+				break;
+			}
 		}
 	}
 	
