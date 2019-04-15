21802fcbbc0c44f00891b4ce544405977285cd56
Jacob Lewis <mediusecho@gmail.com>
2019-04-14 18:49:55 -0700

    Added ParticleEquipEvent

---
+++ ParticleHats/src/com/mediusecho/particlehats/events/EquipEvent.java
@@ -0,0 +1,48 @@
+package com.mediusecho.particlehats.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+import com.mediusecho.particlehats.particles.Hat;
+
+public class EquipEvent extends Event implements Cancellable {
+
+	private static final HandlerList handlers = new HandlerList();
+	
+	private final Player player;
+	private final Hat hat;
+	
+	private boolean cancelled = false;
+	
+	public EquipEvent (final Player player, final Hat hat)
+	{
+		this.player = player;
+		this.hat = hat;
+	}
+	
+	public Player getPlayer () {
+		return player;
+	}
+	
+	public Hat getHat () {
+		return hat;
+	}
+	
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	@Override
+	public boolean isCancelled() {
+		return cancelled;
+	}
+
+	@Override
+	public void setCancelled(boolean cancelled) {
+		this.cancelled = cancelled;
+	}
+
+}
