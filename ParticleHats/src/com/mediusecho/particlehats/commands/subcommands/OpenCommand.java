24b1359fc37be1da3a5dfd3881447498b4f94f77
Jacob Lewis <mediusecho@gmail.com>
2019-04-16 18:39:52 -0700

    Open menu command will now suggest menus the player has permission for, fixed error message being a fixed string

--- ParticleHats/src/com/mediusecho/particlehats/commands/subcommands/OpenCommand.java
+++ ParticleHats/src/com/mediusecho/particlehats/commands/subcommands/OpenCommand.java
@@ -1,14 +1,16 @@
 package com.mediusecho.particlehats.commands.subcommands;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 import com.mediusecho.particlehats.Core;
 import com.mediusecho.particlehats.commands.Command;
-import com.mediusecho.particlehats.commands.CommandPermission;
 import com.mediusecho.particlehats.commands.Sender;
 import com.mediusecho.particlehats.database.Database;
 import com.mediusecho.particlehats.locale.Message;
+import com.mediusecho.particlehats.permission.Permission;
 import com.mediusecho.particlehats.ui.Menu;
 import com.mediusecho.particlehats.ui.MenuInventory;
 import com.mediusecho.particlehats.ui.StaticMenu;
@@ -25,22 +27,46 @@ public class OpenCommand extends Command {
 	@Override
 	public List<String> tabCompelete (Core core, Sender sender, String label, ArrayList<String> args)
 	{
-		return new ArrayList<String>(database.getMenus(false).keySet());
+		if (args.size() == 1) 
+		{
+			Set<String> menus = database.getMenus(false).keySet();
+			List<String> result = new ArrayList<String>();
+			
+			if (sender.hasPermission(getPermission()))
+			{
+				for (String menu : menus)
+				{
+					if (sender.hasPermission(getPermission().append(menu)) || sender.hasPermission(getPermission().append("all"))) {
+						result.add(menu);
+					}
+				}
+			}
+			
+			return result;
+		}
+		return Arrays.asList("");
+		
+		//return new ArrayList<String>(database.getMenus(false).keySet());
 	}
 	
 	@Override
 	public boolean execute(Core core, Sender sender, String label, ArrayList<String> args) 
 	{
-		// TODO: Check against the console
+//		if (!sender.isPlayer())
+//		{
+//			sender.sendMessage(Message.COMMAND_ERROR_PLAYER_ONLY);
+//			return false;
+//		}
 		
 		// No argument
 		if (args.size() == 0)
 		{
-			sender.sendMessage("&cMissing menu name");
+			sender.sendMessage(Message.COMMAND_ERROR_ARGUMENTS);
+			sender.sendMessage(Message.COMMAND_OPEN_USAGE);
 			return false;
 		}
 		
-		if (args.size() == 1)
+		if (args.size() >= 1)
 		{
 			// Grab the name without any extensions
 			String menuName = (args.get(0).contains(".") ? args.get(0).split("\\.")[0] : args.get(0));
@@ -50,7 +76,7 @@ public class OpenCommand extends Command {
 			
 			if (inventory == null)
 			{
-				sender.sendMessage(Message.COMMAND_ERROR_UNKNOWN_MENU.getValue().replace("{1}", menuName));
+				sender.sendMessage(Message.COMMAND_ERROR_UNKNOWN_MENU.replace("{1}", menuName));
 				return false;
 			}
 			
