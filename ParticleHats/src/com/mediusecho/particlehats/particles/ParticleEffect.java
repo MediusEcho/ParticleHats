f1734d4b8f4ff6f10b5c6e73cd81412773c97fb9
Jacob Lewis <mediusecho@gmail.com>
2019-04-23 19:59:22 -0700

    Added supported version

--- ParticleHats/src/com/mediusecho/particlehats/particles/ParticleEffect.java
+++ ParticleHats/src/com/mediusecho/particlehats/particles/ParticleEffect.java
@@ -11,65 +11,66 @@ import org.bukkit.Particle.DustOptions;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.inventory.ItemStack;
 
+import com.mediusecho.particlehats.Core;
 import com.mediusecho.particlehats.locale.Message;
 
 public enum ParticleEffect {
 
-	NONE              (0, 13, "", "", Material.SCUTE),
-	BARRIER           (1, 13, "BARRIER", "barrier", Material.BARRIER),
-	BLOCK_CRACK       (2, 13, "BLOCK_CRACK", "blockcrack", Material.COBBLESTONE),
-	BLOCK_DUST        (3, 13, "BLOCK_DUST", "blockdust", Material.GUNPOWDER),
+	NONE              (0, -1, "", "", Material.SCUTE),
+	BARRIER           (1, 8, "BARRIER", "barrier", Material.BARRIER),
+	BLOCK_CRACK       (2, -1, "BLOCK_CRACK", "blockcrack", Material.COBBLESTONE),
+	BLOCK_DUST        (3, 7, "BLOCK_DUST", "blockdust", Material.GUNPOWDER),
 	BUBBLE_COLUMN_UP  (4, 13, "BUBBLE_COLUMN_UP", "", Material.HORN_CORAL),
 	BUBBLE_POP        (5, 13, "BUBBLE_POP", "", Material.PURPLE_DYE),
-	CLOUD             (6, 13, "CLOUD", "cloud", Material.WHITE_WOOL),
-	CRIT              (7, 13, "CRIT", "crit", Material.DIAMOND_CHESTPLATE),
-	CRIT_MAGIC        (8, 13, "CRIT_MAGIC", "magicCrit", Material.DIAMOND_SWORD),
+	CLOUD             (6, -1, "CLOUD", "cloud", Material.WHITE_WOOL),
+	CRIT              (7, -1, "CRIT", "crit", Material.DIAMOND_CHESTPLATE),
+	CRIT_MAGIC        (8, -1, "CRIT_MAGIC", "magicCrit", Material.DIAMOND_SWORD),
 	CURRENT_DOWN      (9, 13, "CURRENT_DOWN", "", Material.LAPIS_LAZULI),
-	DAMAGE_INDICATOR  (10, 13, "DAMAGE_INDICATOR", "damageIndicator", Material.IRON_SWORD),
-	DRAGON_BREATH     (11, 13, "DRAGON_BREATH", "dragonbreath", Material.DRAGON_HEAD), 
-	DRIP_LAVA         (12, 13, "DRIP_LAVA", "dripLava", Material.LAVA_BUCKET),
-	DRIP_WATER        (13, 13, "DRIP_WATER", "dripWater", Material.WATER_BUCKET),
+	DAMAGE_INDICATOR  (10, 9, "DAMAGE_INDICATOR", "damageIndicator", Material.IRON_SWORD),
+	DRAGON_BREATH     (11, 9, "DRAGON_BREATH", "dragonbreath", Material.DRAGON_HEAD), 
+	DRIP_LAVA         (12, -1, "DRIP_LAVA", "dripLava", Material.LAVA_BUCKET),
+	DRIP_WATER        (13, -1, "DRIP_WATER", "dripWater", Material.WATER_BUCKET),
 	DOLPHIN           (14, 13, "DOLPHIN", "", Material.PRISMARINE_SHARD),
-	ENCHANTMENT_TABLE (15, 13, "ENCHANTMENT_TABLE", "enchantmenttable", Material.ENCHANTING_TABLE),
-	END_ROD           (16, 13, "END_ROD", "endRod", Material.END_ROD),
-	EXPLOSION_HUGE    (17, 13, "EXPLOSION_HUGE", "hugeexplosion", Material.TNT),
-	EXPLOSION_LARGE   (18, 13, "EXPLOSION_LARGE", "largeexplode", Material.TNT),
-	EXPLOSION_NORMAL  (19, 13, "EXPLOSION_NORMAL", "explode", Material.TNT),
-	FALLING_DUST      (20, 13, "FALLING_DUST", "fallingdust", Material.SAND),
-	FIREWORKS_SPARK   (21, 13, "FIREWORKS_SPARK", "fireworksspark", Material.FIREWORK_ROCKET),
-	FLAME             (22, 13, "FLAME", "flame", Material.TORCH),
-	HEART             (23, 13, "HEART", "heart", Material.REDSTONE_BLOCK),
-	ITEM_CRACK        (24, 13, "ITEM_CRACK", "itemcrack", Material.APPLE),
-	LAVA              (25, 13, "LAVA", "lava", Material.MAGMA_BLOCK),
-	MOB_APPEARANCE    (26, 13, "MOB_APPEARANCE", "mobappearance", Material.PLAYER_HEAD),
+	ENCHANTMENT_TABLE (15, -1, "ENCHANTMENT_TABLE", "enchantmenttable", Material.ENCHANTING_TABLE),
+	END_ROD           (16, 9, "END_ROD", "endRod", Material.END_ROD),
+	EXPLOSION_HUGE    (17, -1, "EXPLOSION_HUGE", "hugeexplosion", Material.TNT),
+	EXPLOSION_LARGE   (18, -1, "EXPLOSION_LARGE", "largeexplode", Material.TNT),
+	EXPLOSION_NORMAL  (19, -1, "EXPLOSION_NORMAL", "explode", Material.TNT),
+	FALLING_DUST      (20, 10, "FALLING_DUST", "fallingdust", Material.SAND),
+	FIREWORKS_SPARK   (21, -1, "FIREWORKS_SPARK", "fireworksspark", Material.FIREWORK_ROCKET),
+	FLAME             (22, -1, "FLAME", "flame", Material.TORCH),
+	HEART             (23, -1, "HEART", "heart", Material.REDSTONE_BLOCK),
+	ITEM_CRACK        (24, -1, "ITEM_CRACK", "itemcrack", Material.APPLE),
+	LAVA              (25, -1, "LAVA", "lava", Material.MAGMA_BLOCK),
+	MOB_APPEARANCE    (26, 8, "MOB_APPEARANCE", "mobappearance", Material.PLAYER_HEAD),
 	NAUTILUS          (27, 13, "NAUTILUS", "", Material.CONDUIT),
-	NOTE              (28, 13, "NOTE", "note", Material.NOTE_BLOCK),
-	PORTAL            (29, 13, "PORTAL", "portal", Material.SOUL_SAND),
-	REDSTONE          (30, 13, "REDSTONE", "reddust", Material.REDSTONE),
-	SLIME             (31, 13, "SLIME", "slime", Material.SLIME_BALL),
-	SMOKE_LARGE       (32, 13, "SMOKE_LARGE", "largesmoke", Material.BONE_MEAL),
-	SMOKE_NORMAL      (33, 13, "SMOKE_NORMAL", "smoke", Material.FLINT_AND_STEEL),
-	SNOW_SHOVEL       (34, 13, "SNOW_SHOVEL", "", Material.SNOW_BLOCK),
-	SNOWBALL          (35, 13, "SNOWBALL", "snowballpoof", Material.SNOWBALL),
-	SPELL             (36, 13, "SPELL", "spell", Material.POTION),
-	SPELL_INSTANT     (37, 13, "SPELL_INSTANT", "instantSpell", Material.SPLASH_POTION),
-	SPELL_MOB         (38, 13, "SPELL_MOB", "mobSpell", Material.ZOMBIE_HEAD),
-	SPELL_MOB_AMBIENT (39, 13, "SPELL_MOB_AMBIENT", "mobSpellAmbient", Material.POTION),
-	SPELL_WITCH       (40, 13, "SPELL_WITCH", "witchMagic", Material.SPLASH_POTION),
-	SPIT              (41, 13, "SPIT", "spit", Material.BONE_MEAL),
+	NOTE              (28, -1, "NOTE", "note", Material.NOTE_BLOCK),
+	PORTAL            (29, -1, "PORTAL", "portal", Material.SOUL_SAND),
+	REDSTONE          (30, -1, "REDSTONE", "reddust", Material.REDSTONE),
+	SLIME             (31, -1, "SLIME", "slime", Material.SLIME_BALL),
+	SMOKE_LARGE       (32, -1, "SMOKE_LARGE", "largesmoke", Material.BONE_MEAL),
+	SMOKE_NORMAL      (33, -1, "SMOKE_NORMAL", "smoke", Material.FLINT_AND_STEEL),
+	SNOW_SHOVEL       (34, -1, "SNOW_SHOVEL", "", Material.SNOW_BLOCK),
+	SNOWBALL          (35, -1, "SNOWBALL", "snowballpoof", Material.SNOWBALL),
+	SPELL             (36, -1, "SPELL", "spell", Material.POTION),
+	SPELL_INSTANT     (37, -1, "SPELL_INSTANT", "instantSpell", Material.SPLASH_POTION),
+	SPELL_MOB         (38, -1, "SPELL_MOB", "mobSpell", Material.ZOMBIE_HEAD),
+	SPELL_MOB_AMBIENT (39, -1, "SPELL_MOB_AMBIENT", "mobSpellAmbient", Material.POTION),
+	SPELL_WITCH       (40, -1, "SPELL_WITCH", "witchMagic", Material.SPLASH_POTION),
+	SPIT              (41, 11, "SPIT", "spit", Material.BONE_MEAL),
 	SQUID_INK         (42, 13, "SQUID_INK", "", Material.INK_SAC),
-	SUSPENDED         (43, 13, "SUSPENDED", "suspended", Material.BEDROCK),
-	SUSPENDED_DEPTH   (44, 13, "SUSPENDED_DEPTH", "depthSuspend", Material.BEDROCK),
-	SWEEP_ATTACK      (45, 13, "SWEEP_ATTACK", "sweepAttack", Material.DIAMOND_SWORD),
-	TOTEM             (46, 13, "TOTEM", "totem", Material.TOTEM_OF_UNDYING),
-	TOWN_AURA         (47, 13, "TOWN_AURA", "townaura", Material.BEACON),
-	VILLAGER_ANGRY    (48, 13, "VILLAGER_ANGRY", "angryVillager", Material.WITHER_SKELETON_SKULL),
-	VILLAGER_HAPPY    (49, 13, "VILLAGER_HAPPY", "happyVillager", Material.PLAYER_HEAD),
-	WATER_BUBBLE      (50, 13, "WATER_BUBBLE", "bubble", Material.FISHING_ROD),
-	WATER_DROP        (51, 13, "WATER_DROP", "", Material.WATER_BUCKET),
-	WATER_SPLASH      (52, 13, "WATER_SPLASH", "splash", Material.BIRCH_BOAT),
-	WATER_WAKE        (53, 13, "WATER_WAKE", "wake", Material.OAK_BOAT),
-	ITEMSTACK         (54, 13, "", "", Material.DIAMOND);
+	SUSPENDED         (43, -1, "SUSPENDED", "suspended", Material.BEDROCK),
+	SUSPENDED_DEPTH   (44, -1, "SUSPENDED_DEPTH", "depthSuspend", Material.BEDROCK),
+	SWEEP_ATTACK      (45, 9, "SWEEP_ATTACK", "sweepAttack", Material.DIAMOND_SWORD),
+	TOTEM             (46, 11, "TOTEM", "totem", Material.TOTEM_OF_UNDYING),
+	TOWN_AURA         (47, -1, "TOWN_AURA", "townaura", Material.BEACON),
+	VILLAGER_ANGRY    (48, -1, "VILLAGER_ANGRY", "angryVillager", Material.WITHER_SKELETON_SKULL),
+	VILLAGER_HAPPY    (49, -1, "VILLAGER_HAPPY", "happyVillager", Material.PLAYER_HEAD),
+	WATER_BUBBLE      (50, -1, "WATER_BUBBLE", "bubble", Material.FISHING_ROD),
+	WATER_DROP        (51, 8, "WATER_DROP", "", Material.WATER_BUCKET),
+	WATER_SPLASH      (52, -1, "WATER_SPLASH", "splash", Material.BIRCH_BOAT),
+	WATER_WAKE        (53, 7, "WATER_WAKE", "wake", Material.OAK_BOAT),
+	ITEMSTACK         (54, -1, "", "", Material.DIAMOND);
 	
 	private static final Map<String, ParticleEffect> NAMES   = new HashMap<String, ParticleEffect>();
 	private static final Map<Integer, ParticleEffect> IDS    = new HashMap<Integer, ParticleEffect>();
@@ -166,6 +167,14 @@ public enum ParticleEffect {
 	}
 	
 	/**
+	 * Returns the server version required to use this particle (1.x)
+	 * @return
+	 */
+	public int getRequiredVersion () {
+		return version;
+	}
+	
+	/**
 	 * Get this ParticleEffect's name
 	 * @return The name of this effect as defined in the current messages.yml file
 	 */
@@ -245,6 +254,14 @@ public enum ParticleEffect {
 	 */
 	public ParticleProperty getProperty () {
 		return property;
+	}
+	
+	public boolean isSupported ()
+	{
+		if (version == -1) {
+			return true;
+		}
+		return Core.serverVersion >= version;
 	}
 	
 	/**
