73d3a852b98754088ee73bc2fb62944711aaccda
Jacob Lewis <mediusecho@gmail.com>
2019-05-04 16:52:59 -0700

    Organized values alphabetically, added method of finding closest material match

--- ParticleHats/src/com/mediusecho/particlehats/compatibility/CompatibleMaterial.java
+++ ParticleHats/src/com/mediusecho/particlehats/compatibility/CompatibleMaterial.java
@@ -14,116 +14,168 @@ import com.mediusecho.particlehats.util.ItemUtil;
  * @author MediusEcho
  *
  */
+// TODO: Finish adding alternate materials (1.8 compatible)
 public enum CompatibleMaterial {
 
-	BIRCH_BOAT ("BOAT_BIRCH", 0),
-	OAK_BOAT ("BOAT", 0),
-	
-	INK_SAC ("INK_SACK", 0),
-	ROSE_RED ("INK_SACK", 1),
-	CACTUS_GREEN ("INK_SACK", 2),
-	BROWN_DYE ("INK_SACK", 3),
-	LAPIS_LAZULI ("INK_SACK", 4),
-	PURPLE_DYE ("INK_SACK", 5),
-	CYAN_DYE ("INK_SACK", 6),
-	LIGHT_GRAY_DYE ("INK_SACK", 7),
-	GRAY_DYE ("INK_SACK", 8),
-	PINK_DYE ("INK_SACK", 9),
-	LIME_DYE ("INK_SACK", 10),
-	YELLOW_DYE ("INK_SACK", 11),
-	LIGHT_BLUE_DYE ("INK_SACK", 12),
-	MAGENTA_DYE ("INK_SACK", 13),
-	ORANGE_DYE ("INK_SACK", 14),
-	BONE_MEAL ("INK_SACK", 15),
-	
-	WHITE_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 0),
-	ORANGE_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 1),
-	MAGENTA_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 2),
-	LIGHT_BLUE_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 3),
-	YELLOW_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 4),
-	LIME_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 5),
-	PINK_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 6),
-	GRAY_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 7),
-	LIGHT_GRAY_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 8),
-	CYAN_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 9),
-	PURPLE_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 10),
-	BLUE_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 11),
-	BROWN_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 12),
-	GREEN_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 13),
-	RED_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 14),
-	BLACK_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 15),
-	
-	PRISMARINE_SHARD ("RAW_FISH", 0),
-	REDSTONE_TORCH ("REDSTONE_TORCH_ON", 0),
-	WRITABLE_BOOK ("BOOK_AND_QUILL", 0),
-	EXPERIENCE_BOTTLE ("EXP_BOTTLE", 0),
 	BARRIER ("INK_SACK", 8),
+	BIRCH_BOAT ("BOAT_BIRCH", 0),
+	BLACK_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 15),
+	BLUE_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 11),
+	BONE_MEAL ("INK_SACK", 15),
+	BROWN_DYE ("INK_SACK", 3),
+	BROWN_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 12),
+	
+	CACTUS_GREEN (2, "INK_SACK", "GREEN_DYE"),
+	CAMPFIRE ("TORCH", 0),
 	CAULDRON ("CAULDRON_ITEM", 0),
 	COMPARATOR ("REDSTONE_COMPARATOR", 0),
 	CONDUIT,
+	CYAN_DYE ("INK_SACK", 6),
+	CYAN_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 9),
+	
 	DRAGON_HEAD,
+	
+	EXPERIENCE_BOTTLE ("EXP_BOTTLE", 0),
 	ENCHANTING_TABLE ("ENCHANTMENT_TABLE", 0),
 	END_ROD,
+	
 	FIRE_CHARGE ("FIREBALL", 0),
 	FIREWORK_ROCKET ("FIREWORK_CHARGE", 0),
 	FIREWORK_STAR ("FIREWORK_CHARGE", 0),
+	
+	GRAY_DYE ("INK_SACK", 8),
+	GRAY_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 7),
+	GREEN_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 13),
 	GUNPOWDER ("SULPHUR", 0),
+	
 	HORN_CORAL,
+	
+	INK_SAC (0, "INK_SACK"),
+	
+	LANTERN (0, "TORCH"),
+	LAPIS_LAZULI ("INK_SACK", 4),
+	LIGHT_BLUE_DYE ("INK_SACK", 12),
+	LIGHT_BLUE_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 3),
+	LIGHT_GRAY_DYE ("INK_SACK", 7),
+	LIGHT_GRAY_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 8),
+	LIME_DYE ("INK_SACK", 10),
+	LIME_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 5),
+
+	MAGENTA_DYE ("INK_SACK", 13),
+	MAGENTA_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 2),
 	MAGMA_BLOCK ("MAGMA", 0),
 	MAP ("EMPTY_MAP", 0),
 	MUSHROOM_STEW ("MUSHROOM_SOUP", 0),
+	MUSIC_DISC_CAT ("GREEN_RECORD",  0),
+	MUSIC_DISC_BLOCKS ("RECORD_3", 0),
+	MUSIC_DISC_FAR ("RECORD_5", 0),
 	MUSIC_DISC_STRAD ("RECORD_9", 0),
+	
+	OAK_BOAT ("BOAT", 0),
+	ORANGE_DYE ("INK_SACK", 14),
+	ORANGE_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 1),
+	
+	PINK_DYE ("INK_SACK", 9),
+	PINK_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 6),
 	PLAYER_HEAD ("SKULL_ITEM", 3),
+	PRISMARINE_SHARD ("RAW_FISH", 0),
 	PUFFERFISH ("RAW_FISH", 3),
+	PURPLE_DYE ("INK_SACK", 5),
+	PURPLE_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 10),
+	
+	REDSTONE_TORCH ("REDSTONE_TORCH_ON", 0),
+	RED_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 14),
 	REPEATER ("DIODE", 0),
+	ROSE_RED (1, "INK_SACK", "RED_DYE"),
+
 	SCUTE ("BOWL", 0),
 	SNOWBALL ("SNOW_BALL", 0),
-	SUNFLOWER ("DOUBLE_PLANT", 0),
 	SPLASH_POTION ("POTION", 0),
+	SUNFLOWER ("DOUBLE_PLANT", 0),
+	
 	TOTEM_OF_UNDYING ("TOTEM", 0),
 	TURTLE_HELMET ("LEATHER_HELMET", 0),
-	WITHER_SKELETON_SKULL ("SKULL_ITEM", 1),
+	
 	WHEAT_SEEDS ("SEEDS", 0),
 	WHITE_DYE,
+	WHITE_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 0),
+	WITHER_SKELETON_SKULL ("SKULL_ITEM", 1),
+	WRITABLE_BOOK ("BOOK_AND_QUILL", 0),
+	
+	YELLOW_DYE ("INK_SACK", 11),
+	YELLOW_STAINED_GLASS_PANE ("STAINED_GLASS_PANE", 4),
+	
 	ZOMBIE_HEAD ("SKULL_ITEM", 2);
 	
 	private final Material material;
-	private final int damage;
+	private final int durability;
 	
-	private CompatibleMaterial (String fallback, int damage)
+	/**
+	 * 
+	 * @param durability
+	 * @param aliases
+	 */
+	private CompatibleMaterial (int durability, String... aliases)
 	{
-		this.material = ItemUtil.getMaterial(this.toString(), fallback);
-		this.damage = damage;
+		this.durability = durability;
+		this.material = findClosestMatch(toString(), aliases);
+	}
+	
+	private CompatibleMaterial (String fallback, int durability)
+	{
+		this.material = findClosestMatch(toString(), fallback);
+		this.durability = durability;
 	}
 	
 	private CompatibleMaterial () 
 	{
-		this.material = ItemUtil.getMaterial(this.toString(), "STONE");
-		this.damage = 0;
+		this.material = findClosestMatch(toString(), "STONE");
+		this.durability = 0;
 	}
 	
+	/**
+	 * Get this CompatibleMaterial's compatible Material
+	 * @return
+	 */
 	public Material getMaterial () {
 		return material;
 	}
 	
-	public int getDamage () {
-		return damage;
+	/**
+	 * Get this CompatibleMaterial's durability
+	 * @return
+	 */
+	public int getDurability () {
+		return durability;
 	}
 	
+	/**
+	 * Creates and returns a new ItemStack
+	 * @return
+	 */
 	public ItemStack getItem () {
 		return getItem(1);
 	}
 	
-	@SuppressWarnings("deprecation")
+	/**
+	 * Creates and returns a new ItemStack
+	 * @return
+	 */
 	public ItemStack getItem (int quantity)
 	{
 		if (Core.serverVersion >= 13) {
-			return new ItemStack(material, quantity);
+			return ItemUtil.createItem(material, 1);
 		} else {
-			return new ItemStack(material, quantity, (short) damage);
+			return ItemUtil.createItem(material, (short) durability);
 		}
 	}
 	
+	/**
+	 * Get the CompatibleMaterial that matches this name, or fallback if none are found
+	 * @param name
+	 * @param fallback
+	 * @return
+	 */
 	public static CompatibleMaterial fromName (String name, CompatibleMaterial fallback)
 	{
 		try {
@@ -131,5 +183,29 @@ public enum CompatibleMaterial {
 		} catch (IllegalArgumentException e) {
 			return fallback;
 		}
+	}
+	
+	/**
+	 * Finds the closest material value for the given server version
+	 * @param material
+	 * @param aliases List of alternate names this material could have
+	 * @return
+	 */
+	private Material findClosestMatch (String material, String... aliases)
+	{
+		Material originalMaterial = Material.getMaterial(material);
+		if (originalMaterial != null) {
+			return originalMaterial;
+		}
+		
+		for (String alias : aliases)
+		{
+			Material aliasMaterial = Material.getMaterial(alias);
+			if (aliasMaterial != null) {
+				return aliasMaterial;
+			}
+		}
+		
+		return Material.STONE;
 	}
 }
