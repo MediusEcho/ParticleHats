75ec5e074458f0d3c2537d51414cb017d171d12b
Jacob Lewis <mediusecho@gmail.com>
2019-05-05 13:29:13 -0700

    Added a bunch of alternate methods for #createItem

--- ParticleHats/src/com/mediusecho/particlehats/util/ItemUtil.java
+++ ParticleHats/src/com/mediusecho/particlehats/util/ItemUtil.java
@@ -9,6 +9,8 @@ import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import com.mediusecho.particlehats.Core;
+import com.mediusecho.particlehats.compatibility.CompatibleMaterial;
 import com.mediusecho.particlehats.locale.Message;
 
 public class ItemUtil {
@@ -21,27 +23,97 @@ public class ItemUtil {
 	 * @return
 	 */
 	public static Material getMaterial(String material, Material fallback)
+	{		
+		Material originalMaterial = Material.getMaterial(material);
+		if (material != null) {
+			return originalMaterial;
+		}
+		return fallback;
+//		if (material == null) {
+//			return fallback;
+//		}
+//		
+//		try {
+//			return Material.valueOf(material);
+//		} catch (IllegalArgumentException e) {
+//			return fallback;
+//		}
+	}
+	
+	public static Material getMaterial (String material, String fallback)
 	{
-		if (material == null) {
-			return fallback;
+		Material originalMaterial = Material.getMaterial(material);
+		if (originalMaterial != null) {
+			return originalMaterial;
 		}
 		
-		try {
-			return Material.valueOf(material);
-		} catch (IllegalArgumentException e) {
-			return fallback;
+		Material fallbackMaterial = Material.getMaterial(fallback);
+		if (fallbackMaterial != null) {
+			return fallbackMaterial;
 		}
+		
+		return Material.STONE;
+		
+//		// Try to find the original material
+//		try {
+//			return Material.valueOf(material);
+//		}
+//		
+//		catch (IllegalArgumentException e)
+//		{
+//			// Try to find the fallback material
+//			try {
+//				return Material.valueOf(fallback);
+//			}
+//			
+//			// Try to find the closest match
+//			catch (IllegalArgumentException ex)
+//			{
+//				for (Material mat : Material.values())
+//				{
+//					if (mat.toString().toLowerCase().contains(fallback.toLowerCase())) {
+//						return mat;
+//					}
+//				}
+//			}
+//		}
+//		
+//		// Accept defeat
+//		return Material.STONE;
 	}
 	
 	/**
 	 * Creates and returns a new ItemStack
 	 * @param material
-	 * @param title
-	 * @param description
+	 * @param durability
 	 * @return
 	 */
-	public static ItemStack createItem (Material material, String title, String... description) {
-		return ItemUtil.createItem(material, 1, title, description);
+	@SuppressWarnings("deprecation")
+	public static ItemStack createItem (Material material, short durability)
+	{
+		if (Core.serverVersion < 13) 
+		{
+			ItemStack item = new ItemStack(material, 1, durability);
+			ItemMeta itemMeta = item.getItemMeta();
+			
+			addItemFlags(itemMeta);
+			
+			item.setItemMeta(itemMeta);
+			return item;
+		}
+		return createItem(material, 1);
+	}
+	
+	/**
+	 * Creates and returns a new ItemStack
+	 * @param material
+	 * @param durability
+	 * @return
+	 */
+	public static ItemStack createItem (String material, short durability)
+	{
+		Material itemMaterial = getMaterial(material, Material.STONE);
+		return createItem(itemMaterial, durability);
 	}
 	
 	/**
@@ -52,52 +124,14 @@ public class ItemUtil {
 	 * @param description
 	 * @return
 	 */
-	public static ItemStack createItem (Material material, int quantity, String title, String... description)
+	public static ItemStack createItem (Material material, int quantity, String title, List<String> description)
 	{
 		ItemStack item = new ItemStack(material, quantity);
-		ItemMeta itemMeta = item.getItemMeta();
-		
-		itemMeta.setDisplayName(StringUtil.colorize(title));
-		itemMeta.setLore(StringUtil.colorize(Arrays.asList(description)));
-		itemMeta.addItemFlags(ItemFlag.values());
-		
-		item.setItemMeta(itemMeta);
-		return item;
-	}
-	
-	/**
-	 * Creates and returns a new ItemStack
-	 * @param material
-	 * @param quantity
-	 * @param title
-	 * @param description
-	 * @return
-	 */
-	public static ItemStack createItem (Material material, String title, List<String> description)
-	{
-		ItemStack item = new ItemStack(material);
 		ItemMeta itemMeta = item.getItemMeta();
 		
 		itemMeta.setDisplayName(StringUtil.colorize(title));
 		itemMeta.setLore(StringUtil.colorize(description));
-		itemMeta.addItemFlags(ItemFlag.values());
-		
-		item.setItemMeta(itemMeta);
-		return item;
-	}
-	
-	/**
-	 * Creates and returns a new ItemStack
-	 * @param material
-	 * @param quantity
-	 * @return
-	 */
-	public static ItemStack createItem (Material material, int quantity)
-	{
-		ItemStack item = new ItemStack(material, quantity);
-		ItemMeta itemMeta = item.getItemMeta();
-		
-		itemMeta.addItemFlags(ItemFlag.values());
+		addItemFlags(itemMeta);
 		
 		item.setItemMeta(itemMeta);
 		return item;
@@ -115,25 +149,122 @@ public class ItemUtil {
 		ItemStack item = new ItemStack(material, quantity);
 		ItemMeta itemMeta = item.getItemMeta();
 		
-		itemMeta.setDisplayName(StringUtil.colorize(title));
-		itemMeta.addItemFlags(ItemFlag.values());
+		itemMeta.setDisplayName(title);
+		addItemFlags(itemMeta);
 		
 		item.setItemMeta(itemMeta);
 		return item;
 	}
 	
 	/**
-	 * Creates a new ItemStack
+	 * Creates and returns a new ItemStack
+	 * @param material
+	 * @param quantity
+	 * @return
+	 */
+	public static ItemStack createItem (Material material, int quantity)
+	{
+		ItemStack item = new ItemStack(material, quantity);
+		ItemMeta itemMeta = item.getItemMeta();
+		
+		addItemFlags(itemMeta);
+		
+		item.setItemMeta(itemMeta);
+		return item;
+	}
+	
+	/**
+	 * Creates and returns a new ItemStack
+	 * @param material
+	 * @param title
+	 * @param description
+	 * @return
+	 */
+	public static ItemStack createItem (Material material, String title, String... description) {
+		return ItemUtil.createItem(material, 1, title, Arrays.asList(description));
+	}
+	
+	/**
+	 * Creates and returns a new ItemStack
+	 * @param material
+	 * @param quantity
+	 * @param title
+	 * @param description
+	 * @return
+	 */
+	public static ItemStack createItem (Material material, int quantity, String title, String... description) {
+		return createItem(material, quantity, title, Arrays.asList(description));
+	}
+	
+	/**
+	 * Creates and returns a new ItemStack
+	 * @param material
+	 * @param quantity
+	 * @param title
+	 * @param description
+	 * @return
+	 */
+	public static ItemStack createItem (Material material, String title, List<String> description) {
+		return createItem(material, 1, title, description);
+	}
+	
+	/**
+	 * Creates and returns a new ItemStack
+	 * @param material
+	 * @param quantity
+	 * @param title
+	 * @param description
+	 * @return
+	 */
+	public static ItemStack createItem (Material material, int quantity, Message title, Message description) {
+		return createItem(material, quantity, title.getValue(), StringUtil.parseDescription(description.getValue()));
+	}
+	
+	/**
+	 * Creates and returns a new ItemStack
+	 * @param material
+	 * @param title
+	 * @param description
+	 * @return
+	 */
+	public static ItemStack createItem (Material material, Message title, Message description) {
+		return createItem(material, 1, title.getValue(), StringUtil.parseDescription(description.getValue()));
+	}
+	
+	/**
+	 * Creates and returns a new ItemStack
+	 * @param material
+	 * @param title
+	 * @param description
+	 * @return
+	 */
+	public static ItemStack createItem (Material material, String title, Message description) {
+		return createItem(material, 1, title, StringUtil.parseDescription(description.getValue()));
+	}
+	
+	/**
+	 * Creates and returns a new ItemStack
+	 * @param material
+	 * @param title
+	 * @param description
+	 * @return
+	 */
+	public static ItemStack createItem (Material material, Message title, String... description) {
+		return createItem(material, 1, title.getValue(), description);
+	}
+	
+	/**
+	 * Creates and returns a new ItemStack
 	 * @param material
 	 * @param title
 	 * @return
 	 */
 	public static ItemStack createItem (Material material, String title) {
-		return ItemUtil.createItem(material, 1, title);
+		return createItem(material, 1, title);
 	}
 	
 	/**
-	 * Creates a new ItemStack
+	 * Creates and returns a new ItemStack
 	 * @param material
 	 * @param title
 	 * @return
@@ -143,42 +274,164 @@ public class ItemUtil {
 	}
 	
 	/**
-	 * 
+	 * Creates and returns a new ItemStack
 	 * @param material
+	 * @param quantity
 	 * @param title
 	 * @param description
 	 * @return
 	 */
-	public static ItemStack createItem (Material material, Message title, Message description) {
-		return createItem(material, title.getValue(), StringUtil.parseDescription(description.getValue()));
-	}
-	
-	/**
-	 * 
-	 * @param material
-	 * @param title
-	 * @param description
-	 * @return
-	 */
-	public static ItemStack createItem (Material material, String title, Message description) {
-		return createItem(material, title, StringUtil.parseDescription(description.getValue()));
-	}
-	
-	/**
-	 * Sets this items decription
-	 * @param item
-	 * @param description
-	 */
-	public static void setItemDescription (ItemStack item, String... description)
+	public static ItemStack createItem (CompatibleMaterial material, int quantity, String title, List<String> description)
 	{
+		ItemStack item = material.getItem(quantity);
 		ItemMeta itemMeta = item.getItemMeta();
 		
-		itemMeta.setLore(StringUtil.colorize(Arrays.asList(description)));
+		itemMeta.setDisplayName(StringUtil.colorize(title));
+		itemMeta.setLore(StringUtil.colorize(description));
+		addItemFlags(itemMeta);
+		
 		item.setItemMeta(itemMeta);
+		return item;
 	}
 	
 	/**
-	 * Sets this items decription
+	 * Creates and returns a new ItemStack
+	 * @param material
+	 * @param quantity
+	 * @param title
+	 * @return
+	 */
+	public static ItemStack createItem (CompatibleMaterial material, int quantity, String title)
+	{
+		ItemStack item = material.getItem(quantity);
+		ItemMeta itemMeta = item.getItemMeta();
+		
+		itemMeta.setDisplayName(title);
+		addItemFlags(itemMeta);
+		
+		item.setItemMeta(itemMeta);
+		return item;
+	}
+	
+	/**
+	 * Creates and returns a new ItemStack
+	 * @param material
+	 * @param quantity
+	 * @return
+	 */
+	public static ItemStack createItem (CompatibleMaterial material, int quantity)
+	{
+		ItemStack item = material.getItem(quantity);
+		ItemMeta itemMeta = item.getItemMeta();
+		
+		addItemFlags(itemMeta);
+		
+		item.setItemMeta(itemMeta);
+		return item;
+	}
+	
+	/**
+	 * Creates and returns a new ItemStack
+	 * @param material
+	 * @param title
+	 * @param description
+	 * @return
+	 */
+	public static ItemStack createItem (CompatibleMaterial material, String title, String... description) {
+		return createItem(material, 1, title, Arrays.asList(description));
+	}
+	
+	/**
+	 * Creates and returns a new ItemStack
+	 * @param material
+	 * @param quantity
+	 * @param title
+	 * @param description
+	 * @return
+	 */
+	public static ItemStack createItem (CompatibleMaterial material, int quantity, String title, String... description) {
+		return createItem(material, quantity, title, Arrays.asList(description));
+	}
+	
+	/**
+	 * Creates and returns a new ItemStack
+	 * @param material
+	 * @param quantity
+	 * @param title
+	 * @param description
+	 * @return
+	 */
+	public static ItemStack createItem (CompatibleMaterial material, String title, List<String> description) {
+		return createItem(material, 1, title, description);
+	}
+	
+	/**
+	 * Creates and returns a new ItemStack
+	 * @param material
+	 * @param quantity
+	 * @param title
+	 * @param description
+	 * @return
+	 */
+	public static ItemStack createItem (CompatibleMaterial material, int quantity, Message title, Message description) {
+		return createItem(material, quantity, title.getValue(), StringUtil.parseDescription(description.getValue()));
+	}
+	
+	/**
+	 * Creates and returns a new ItemStack
+	 * @param material
+	 * @param title
+	 * @param description
+	 * @return
+	 */
+	public static ItemStack createItem (CompatibleMaterial material, Message title, Message description) {
+		return createItem(material, 1, title.getValue(), StringUtil.parseDescription(description.getValue()));
+	}
+	
+	/**
+	 * Creates and returns a new ItemStack
+	 * @param material
+	 * @param title
+	 * @param description
+	 * @return
+	 */
+	public static ItemStack createItem (CompatibleMaterial material, String title, Message description) {
+		return createItem(material, 1, title, StringUtil.parseDescription(description.getValue()));
+	}
+	
+	/**
+	 * Creates and returns a new ItemStack
+	 * @param material
+	 * @param title
+	 * @param description
+	 * @return
+	 */
+	public static ItemStack createItem (CompatibleMaterial material, Message title, String... description) {
+		return createItem(material, 1, title.getValue(), description);
+	}
+	
+	/**
+	 * Creates and returns a new ItemStack
+	 * @param material
+	 * @param title
+	 * @return
+	 */
+	public static ItemStack createItem (CompatibleMaterial material, String title) {
+		return createItem(material, 1, title);
+	}
+	
+	/**
+	 * Creates and returns a new ItemStack
+	 * @param material
+	 * @param title
+	 * @return
+	 */
+	public static ItemStack createItem (CompatibleMaterial material, Message title) {
+		return createItem(material, 1, title.getValue());
+	}
+	
+	/**
+	 * Sets this item's description
 	 * @param item
 	 * @param description
 	 */
@@ -191,7 +444,16 @@ public class ItemUtil {
 	}
 	
 	/**
-	 * Sets this items displayName
+	 * Sets this item's description
+	 * @param item
+	 * @param description
+	 */
+	public static void setItemDescription (ItemStack item, String... description) {
+		setItemDescription(item, Arrays.asList(description));
+	}
+	
+	/**
+	 * Sets this item's name
 	 * @param item
 	 * @param name
 	 */
@@ -203,17 +465,48 @@ public class ItemUtil {
 		item.setItemMeta(itemMeta);
 	}
 	
+	/**
+	 * Sets this item's name
+	 * @param item
+	 * @param name
+	 */
+	public static void setItemName (ItemStack item, Message name) {
+		setItemName(item, name.getValue());
+	}
+	
+	/**
+	 * Sets this item's name and description
+	 * @param item
+	 * @param name
+	 * @param description
+	 */
 	public static void setNameAndDescription (ItemStack item, String name, List<String> description)
 	{
 		ItemMeta itemMeta = item.getItemMeta();
 		
 		itemMeta.setDisplayName(StringUtil.colorize(name));
 		itemMeta.setLore(description);
-		itemMeta.addItemFlags(ItemFlag.values());
+		addItemFlags(itemMeta);
 		
 		item.setItemMeta(itemMeta);
 	}
 	
+	/**
+	 * Sets this item's name and description
+	 * @param item
+	 * @param name
+	 * @param description
+	 */
+	public static void setNameAndDescription (ItemStack item, Message name, Message description) {
+		setNameAndDescription(item, name.getValue(), StringUtil.parseDescription(description.getValue()));
+	}
+	
+	/**
+	 * Sets this item's name and description
+	 * @param item
+	 * @param name
+	 * @param description
+	 */
 	public static void setNameAndDescription (ItemStack item, String name, String... description) {
 		setNameAndDescription(item, name, Arrays.asList(description));
 	}
@@ -224,13 +517,20 @@ public class ItemUtil {
 	 */
 	public static void highlightItem (ItemStack item)
 	{
-		ItemMeta itemMeta = item.getItemMeta();
-		itemMeta.addItemFlags(ItemFlag.values());
-		itemMeta.addEnchant(Enchantment.ARROW_DAMAGE, 0, true);
-		
-		item.setItemMeta(itemMeta);
+		if (Core.serverVersion >= 8) 
+		{
+			ItemMeta itemMeta = item.getItemMeta();
+			addItemFlags(itemMeta);
+			itemMeta.addEnchant(Enchantment.ARROW_DAMAGE, 0, true);
+			
+			item.setItemMeta(itemMeta);
+		}
 	}
 	
+	/**
+	 * Removes an items enchanted glow
+	 * @param item
+	 */
 	public static void stripHighlight (ItemStack item)
 	{
 		ItemMeta itemMeta = item.getItemMeta();
@@ -240,21 +540,63 @@ public class ItemUtil {
 	}
 	
 	/**
-	 * Returns a Material matching the materialName, or the fallback material if that fails
-	 * @param materialName
-	 * @param fallbackMaterial
-	 * @return
+	 * Sets this item's material type and durability
+	 * @param item
+	 * @param material
+	 * @param damage
 	 */
-	public static Material materialFromString (String materialName, Material fallbackMaterial)
+	@SuppressWarnings("deprecation")
+	public static void setItemType (ItemStack item, Material material, short damage) 
 	{
-		if (materialName == null) {
-			return fallbackMaterial;
-		}
+		item.setType(material);
 		
-		try {
-			return Material.valueOf(materialName);
-		} catch (IllegalArgumentException e) {
-			return fallbackMaterial;
+		if (Core.serverVersion < 13) {
+			item.setDurability(damage);
 		}
+	}
+	
+	/**
+	 * Sets this item's material type and durability
+	 * @param item
+	 * @param material
+	 * @param damage
+	 */
+	public static void setItemType (ItemStack item, Material material, int damage) {
+		setItemType(item, material, (short) damage);
+	}
+	
+	/**
+	 * Sets this item's material type and durability
+	 * @param item
+	 * @param material
+	 */
+	public static void setItemType (ItemStack item, CompatibleMaterial material) {
+		setItemType(item, material.getMaterial(), material.getDurability());
+	}
+	
+	/**
+	 * Sets this item's material type and durability
+	 * @param item
+	 * @param i
+	 */
+	@SuppressWarnings("deprecation")
+	public static void setItemType (ItemStack item, ItemStack i) 
+	{
+		if (Core.serverVersion < 13) {
+			setItemType(item, i.getType(), i.getDurability());
+		} else {
+			item.setType(i.getType());
+		}
+	}
+	
+	/**
+	 * Attempts to apply ItemFlag values to this ItemStack
+	 * @param itemMeta
+	 */
+	private static void addItemFlags (ItemMeta itemMeta)
+	{
+		try {
+			itemMeta.addItemFlags(ItemFlag.values());
+		} catch (NoClassDefFoundError e) {}
 	}
 }
