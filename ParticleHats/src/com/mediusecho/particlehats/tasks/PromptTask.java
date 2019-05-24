2b14784af423d3e068bed410e3b71685611d1edb
Jacob Lewis <mediusecho@gmail.com>
2019-05-24 16:38:19 -0700

    removed commented out code

--- ParticleHats/src/com/mediusecho/particlehats/tasks/PromptTask.java
+++ ParticleHats/src/com/mediusecho/particlehats/tasks/PromptTask.java
@@ -8,14 +8,8 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 import com.mediusecho.particlehats.ParticleHats;
 import com.mediusecho.particlehats.editor.MetaState;
-import com.mediusecho.particlehats.managers.SettingsManager;
 import com.mediusecho.particlehats.player.PlayerState;
 import com.mediusecho.particlehats.prompt.Prompt;
-import com.mediusecho.particlehats.util.StringUtil;
-
-import net.md_5.bungee.api.ChatMessageType;
-import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.TextComponent;
 
 public class PromptTask extends BukkitRunnable {
 
@@ -47,31 +41,7 @@ public class PromptTask extends BukkitRunnable {
 				if (prompt.canPrompt(passes)) {
 					prompt.prompt(player, metaState);
 				}
-				
-//				if (core.canUseBungee() && SettingsManager.EDITOR_USE_ACTION_BAR.getBoolean())
-//				{
-//					int time = playerState.getMetaStateTime();
-//					if (time <= 0) 
-//					{
-//						metaState.reopenEditor(playerState.getMenuBuilder());
-//						continue;
-//					}
-//					
-//					String description = metaState.getDescription() + StringUtil.colorize(" &f(" + time + ")");
-//					prompt(player, description);
-//				}
 			}
 		}
 	}
-
-//	/**
-//	 * Sends the player a message using their Action Bar
-//	 * @param player
-//	 * @param message
-//	 */
-//	public void prompt (Player player, String message)
-//	{
-//		BaseComponent[] bc = TextComponent.fromLegacyText(message);
-//		player.spigot().sendMessage(ChatMessageType.ACTION_BAR, bc);
-//	}
 }
