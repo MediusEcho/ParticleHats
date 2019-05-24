b0054d1d2dc212a2024b04713dd3c815729d679e
Jacob Lewis <mediusecho@gmail.com>
2019-05-23 18:05:39 -0700

    Added search option in icon menu

--- ParticleHats/src/com/mediusecho/particlehats/editor/menus/EditorIconMenu.java
+++ ParticleHats/src/com/mediusecho/particlehats/editor/menus/EditorIconMenu.java
@@ -1,6 +1,7 @@
 package com.mediusecho.particlehats.editor.menus;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
@@ -9,6 +10,7 @@ import com.mediusecho.particlehats.ParticleHats;
 import com.mediusecho.particlehats.compatibility.CompatibleMaterial;
 import com.mediusecho.particlehats.editor.EditorMenu;
 import com.mediusecho.particlehats.editor.MenuBuilder;
+import com.mediusecho.particlehats.editor.MetaState;
 import com.mediusecho.particlehats.locale.Message;
 import com.mediusecho.particlehats.util.ItemUtil;
 import com.mediusecho.particlehats.util.StringUtil;
@@ -19,6 +21,7 @@ public class EditorIconMenu extends EditorMenu {
 	
 	private final Message itemName;
 	private final Message itemDescription;
+	private boolean searching = false;
 	
 	public EditorIconMenu(ParticleHats core, Player owner, MenuBuilder menuBuilder, Message title, Message name, Message description, EditorItemCallback itemCallback) 
 	{
@@ -41,6 +44,30 @@ public class EditorIconMenu extends EditorMenu {
 		menuBuilder.goBack();
 		return EditorClickType.NEUTRAL;
 	}
+	
+	@Override
+	public void open ()
+	{
+		super.open();
+		
+		if (searching)
+		{
+			String blockName = menuBuilder.getMetaArgument();
+			if (!blockName.equals("")) 
+			{
+				menuBuilder.resetMetaArgument();
+				EditorSearchMenu editorSearchMenu = new EditorSearchMenu(core, owner, menuBuilder, blockName, (item) ->
+				{
+					itemCallback.onSelect(item);
+					menuBuilder.goBack();
+				});
+				menuBuilder.addMenu(editorSearchMenu);
+				editorSearchMenu.open();
+			}
+			
+			searching = false;
+		}
+	}
 
 	@Override
 	protected void build() 
@@ -55,6 +82,16 @@ public class EditorIconMenu extends EditorMenu {
 			}
 			setItem(i, info);
 		}
+		
+		setButton(26, ItemUtil.createItem(Material.SIGN, Message.EDITOR_MISC_SEARCH), (event, slot) ->
+		{
+			searching = true;
+			menuBuilder.setOwnerState(MetaState.BLOCK_SEARCH);
+			core.prompt(owner, MetaState.HAT_NAME);
+			owner.closeInventory();
+			
+			return EditorClickType.NEUTRAL;
+		});
 		
 		setButton(13, backButton, backAction);
 	}
