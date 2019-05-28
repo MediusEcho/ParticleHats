8d42ed711c0fc2671b264b37b26771d0a043257d
Jacob Lewis <mediusecho@gmail.com>
2019-05-28 14:14:32 -0700

    added particlehats effect

---
+++ ParticleHats/src/com/mediusecho/particlehats/particles/effects/ParticleHatsEffect.java
@@ -0,0 +1,12 @@
+package com.mediusecho.particlehats.particles.effects;
+
+import com.mediusecho.particlehats.util.ResourceUtil;
+
+public class ParticleHatsEffect extends CommunityEffect {
+
+	public ParticleHatsEffect() 
+	{
+		super(ResourceUtil.getImage("particlehats.png"), "particlehats", "&6ParticleHats", "&7Medius_Echo");
+	}
+
+}
