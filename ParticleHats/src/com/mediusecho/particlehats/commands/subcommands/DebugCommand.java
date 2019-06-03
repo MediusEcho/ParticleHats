98ebe29e183c5c43c3a06026bbc91e8198bd1236
Jacob Lewis <mediusecho@gmail.com>
2019-06-02 18:22:51 -0700

    updated debug command

--- ParticleHats/src/com/mediusecho/particlehats/commands/subcommands/DebugCommand.java
+++ ParticleHats/src/com/mediusecho/particlehats/commands/subcommands/DebugCommand.java
@@ -1,22 +1,33 @@
 package com.mediusecho.particlehats.commands.subcommands;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import com.mediusecho.particlehats.ParticleHats;
 import com.mediusecho.particlehats.commands.Command;
 import com.mediusecho.particlehats.commands.Sender;
+import com.mediusecho.particlehats.database.properties.Group;
 import com.mediusecho.particlehats.locale.Message;
 import com.mediusecho.particlehats.permission.Permission;
-import com.mediusecho.particlehats.util.MathUtil;
 
 public class DebugCommand extends Command {
 
 	@Override
 	public boolean execute(ParticleHats core, Sender sender, String label, ArrayList<String> args) 
 	{
-		//sender.sendMessage("\U+2588");
-		double value = Double.valueOf(args.get(0));
-		sender.sendMessage(Double.toString(MathUtil.wrapAngle(value)));
+		List<Group> groups = new ArrayList<Group>();
+		groups.add(new Group("test", "hello", 5));
+		groups.add(new Group("test_1", "world", 1));
+		groups.add(new Group("default", "particles", 100));
+		
+		Collections.sort(groups, (g1, g2) -> {
+			return g1.getWeight() > g2.getWeight() ? 1 : -1;
+		});
+		
+		for (Group g : groups) {
+			ParticleHats.debug("name: " + g.getName() + " menu: " + g.getDefaultMenu() + " weight: " + g.getWeight());
+		}
 		
 		return false;
 	}
