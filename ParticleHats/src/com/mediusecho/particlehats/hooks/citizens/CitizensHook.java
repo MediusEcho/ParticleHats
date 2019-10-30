a453cf47104d081513fba1a8e81f07d800fe1ddd
Jacob Lewis <mediusecho@gmail.com>
2019-10-29 19:00:37 -0700

    Started early work on CitizensHook

--- ParticleHats/src/com/mediusecho/particlehats/hooks/citizens/CitizensHook.java
+++ ParticleHats/src/com/mediusecho/particlehats/hooks/citizens/CitizensHook.java
@@ -1,11 +1,18 @@
 package com.mediusecho.particlehats.hooks.citizens;
 
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 import com.mediusecho.particlehats.ParticleHats;
-import com.mediusecho.particlehats.particles.Hat;
+import com.mediusecho.particlehats.editor.MetaState;
+import com.mediusecho.particlehats.editor.citizens.CitizensMenuManager;
+import com.mediusecho.particlehats.player.EntityState;
+import com.mediusecho.particlehats.player.PlayerState;
 
+import net.citizensnpcs.api.CitizensAPI;
+import net.citizensnpcs.api.event.NPCDespawnEvent;
 import net.citizensnpcs.api.event.NPCRightClickEvent;
 import net.citizensnpcs.api.npc.NPC;
 
@@ -22,13 +29,39 @@ public class CitizensHook implements Listener {
 	@EventHandler
 	public void onNPCRightClick (NPCRightClickEvent event)
 	{
-		NPC npc = event.getNPC();
-		ParticleHats.log("NPCRightClickEvent called, id: " + npc.getUniqueId().toString());
+		Player player = event.getClicker();
+		EntityState entityState = plugin.getEntityState(player);
 		
-		Hat hat = plugin.getDatabase().getHatFromLabel("npc_test");
-		if (hat != null) {
-			plugin.getEntityState(npc.getEntity()).addHat(hat);
+		if (entityState instanceof PlayerState)
+		{
+			PlayerState playerState = (PlayerState)entityState;
+			
+			if (playerState.getMetaState() == MetaState.NPC_MANAGE)
+			{	
+				CitizensMenuManager citizensManager = new CitizensMenuManager(plugin, player, event.getNPC().getEntity());
+				
+				playerState.setMetaState(MetaState.NONE);
+				playerState.setMenuManager(citizensManager);
+				
+				citizensManager.open();
+			}
 		}
+	}
+	
+	@EventHandler
+	public void onNPCDespawn (NPCDespawnEvent event)
+	{
+		Entity entity = event.getNPC().getEntity();
+		plugin.removePlayerState(entity.getUniqueId());
+	}
+	
+	public Entity getNPCEntity (int id)
+	{
+		NPC npc = CitizensAPI.getNPCRegistry().getById(id);
+		if (npc == null) {
+			return null;
+		}
+		return npc.getEntity();
 	}
 	
 }
