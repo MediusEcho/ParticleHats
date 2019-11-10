49d22c55b2a1750787cbd94d9b98141ecdcc4107
Jacob Lewis <mediusecho@gmail.com>
2019-11-10 15:07:35 -0800

    changed active particles menu to equipped particles menu

---
+++ ParticleHats/src/com/mediusecho/particlehats/ui/EquippedParticlesMenu.java
@@ -0,0 +1,144 @@
+package com.mediusecho.particlehats.ui;
+
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import com.mediusecho.particlehats.ParticleHats;
+import com.mediusecho.particlehats.editor.EditorLore;
+import com.mediusecho.particlehats.locale.Message;
+import com.mediusecho.particlehats.particles.Hat;
+import com.mediusecho.particlehats.util.ItemUtil;
+import com.mediusecho.particlehats.player.PlayerState;
+
+public class EquippedParticlesMenu extends AbstractListMenu {
+
+	final boolean fromMenu;
+	final PlayerState playerState;
+	final MenuAction hatAction;
+	
+	public EquippedParticlesMenu(ParticleHats core, MenuManager menuManager, Player owner, boolean fromMenu) 
+	{
+		super(core, menuManager, owner, true);
+		
+		this.fromMenu = fromMenu;
+		this.playerState = core.getPlayerState(owner);
+		this.totalPages = 1;
+		this.setMenu(0, Bukkit.createInventory(null, 54, Message.ACTIVE_PARTICLES_MENU_TITLE.getValue()));
+		
+		hatAction = (event, slot) -> 
+		{	
+			int index = getClampedIndex(slot, 10, 2);
+			if (index >= playerState.getHatCount()) {
+				return MenuClickResult.NONE;
+			}
+			
+			Hat hat = playerState.getActiveHats().get(index);
+			if (hat == null) {
+				return MenuClickResult.NONE;
+			}
+			
+			if (event.isLeftClick())
+			{
+				hat.setHidden(!hat.isHidden());
+				
+				ItemStack item = menus.get(currentPage).getItem(slot);
+				EditorLore.updateActiveHatDescription(item, hat);
+				
+				if (hat.isHidden()) {
+					ItemUtil.stripHighlight(item);
+				} else {
+					ItemUtil.highlightItem(item);
+				}
+			}
+			
+			else if (event.isShiftRightClick()) 
+			{
+				playerState.removeHat(index);
+				deleteSlot(currentPage, slot);
+			}
+			
+			return MenuClickResult.NEUTRAL;
+		};
+		
+		build();
+	}
+	
+	@Override
+	public void insertEmptyItem () {
+		
+	}
+	
+	@Override
+	public void removeEmptyItem () {
+		setButton(0, 22, null, hatAction);
+	}
+
+	@Override
+	protected void build() 
+	{				
+		for (int i = 0; i < 28; i++) {
+			setAction(getNormalIndex(i, 10, 2), hatAction);
+		}
+		
+		if (fromMenu)
+		{
+			setButton(0, 49, ItemUtil.createItem(Material.NETHER_STAR, Message.EDITOR_MISC_GO_BACK), (event, slot) -> 
+			{
+				StaticMenuManager staticManager = (StaticMenuManager)playerState.getMenuManager();
+				if (staticManager == null) {
+					return MenuClickResult.NONE;
+				}
+				
+				AbstractMenu previousMenu = staticManager.getPreviousOpenMenu();
+				if (previousMenu == null) {
+					return MenuClickResult.NONE;
+				}
+				
+				previousMenu.open();
+				return MenuClickResult.NEUTRAL;
+			});
+		}
+		
+		else
+		{
+			// TODO: Finish porting the rest of the active particles menu
+		}
+		
+		List<Hat> equippedHats = playerState.getActiveHats();
+		if (equippedHats.size() == 0) 
+		{
+			setEmpty(true);
+			return;
+		}
+		
+		int index = 0;
+		for (Hat hat : equippedHats)
+		{
+			ItemStack item = hat.getItem();
+			EditorLore.updateActiveHatDescription(item, hat);
+			
+			if (!hat.isHidden()) {
+				ItemUtil.highlightItem(item);
+			} else {
+				ItemUtil.stripHighlight(item);
+			}
+			
+			setItem(0, getNormalIndex(index++, 10, 2), item);
+		}
+	}
+
+	@Override
+	public void onClose(boolean forced) {}
+
+	@Override
+	public void onTick(int ticks) {}
+
+	@Override
+	public String getName () {
+		return "EquippedParticles";
+	}
+}
