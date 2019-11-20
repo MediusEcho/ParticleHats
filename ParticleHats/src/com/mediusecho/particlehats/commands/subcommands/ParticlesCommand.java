201caa18c0042c104aff7f38162beeacde1a7e87
Jacob Lewis <mediusecho@gmail.com>
2019-11-20 13:43:19 -0800

    tweaked particles command to use new menu system

--- ParticleHats/src/com/mediusecho/particlehats/commands/subcommands/ParticlesCommand.java
+++ ParticleHats/src/com/mediusecho/particlehats/commands/subcommands/ParticlesCommand.java
@@ -8,28 +8,28 @@ import com.mediusecho.particlehats.commands.Sender;
 import com.mediusecho.particlehats.locale.Message;
 import com.mediusecho.particlehats.permission.Permission;
 import com.mediusecho.particlehats.player.PlayerState;
-import com.mediusecho.particlehats.ui.ActiveParticlesMenu;
-import com.mediusecho.particlehats.ui.GuiState;
+import com.mediusecho.particlehats.ui.EquippedParticlesMenu;
+import com.mediusecho.particlehats.ui.StaticMenuManager;
 
 public class ParticlesCommand extends Command {
 
 	@Override
 	public boolean execute(ParticleHats core, Sender sender, String label, ArrayList<String> args) 
 	{		
+		// TODO: Rewrite particles command
 		PlayerState playerState = core.getPlayerState(sender.getPlayer());
 		
-		if (playerState.isEditing()) 
+		if (playerState.hasEditorOpen()) 
 		{
 			sender.sendMessage(Message.COMMAND_ERROR_ALREADY_EDITING);
 			return false;
 		}
 		
-		ActiveParticlesMenu activeParticlesMenu = new ActiveParticlesMenu(core, sender.getPlayer(), false);
+		StaticMenuManager staticManager = core.getMenuManagerFactory().getStaticMenuManager(playerState);
+		EquippedParticlesMenu particlesMenu = new EquippedParticlesMenu(core, staticManager, sender.getPlayer(), false);
 		
-		playerState.setOpenMenu(activeParticlesMenu, false);
-		playerState.setGuiState(GuiState.SWITCHING_MENU);
-		
-		activeParticlesMenu.open();
+		staticManager.addMenu(particlesMenu);
+		particlesMenu.open();
 		return true;
 	}
 
