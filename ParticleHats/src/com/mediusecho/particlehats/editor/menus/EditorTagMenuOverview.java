f2a9c55e86b174838ca6adcafd017db3a77d3a59
Jacob Lewis <mediusecho@gmail.com>
2019-11-20 09:28:32 -0800

    renamed TagOverviewMenu to TagMenuOverview, rewrote tag menu to use new abstract menu

---
+++ ParticleHats/src/com/mediusecho/particlehats/editor/menus/EditorTagMenuOverview.java
@@ -0,0 +1,159 @@
+package com.mediusecho.particlehats.editor.menus;
+
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import com.mediusecho.particlehats.ParticleHats;
+import com.mediusecho.particlehats.compatibility.CompatibleMaterial;
+import com.mediusecho.particlehats.database.Database.DataType;
+import com.mediusecho.particlehats.editor.EditorMenuManager;
+import com.mediusecho.particlehats.locale.Message;
+import com.mediusecho.particlehats.particles.Hat;
+import com.mediusecho.particlehats.particles.properties.ParticleTag;
+import com.mediusecho.particlehats.ui.AbstractListMenu;
+import com.mediusecho.particlehats.util.ItemUtil;
+
+public class EditorTagMenuOverview extends AbstractListMenu {
+
+	private final EditorMenuManager editorManager;
+	private final Hat targetHat;
+	private final String tagTitle = Message.EDITOR_TAG_OVERVIEW_MENU_TAG_TITLE.getValue();
+	private final ItemStack emptyItem = ItemUtil.createItem(CompatibleMaterial.BARRIER, Message.EDITOR_TAG_OVERVIEW_MENU_EMPTY);
+	
+	private boolean isModified = false;
+	
+	public EditorTagMenuOverview(ParticleHats core, EditorMenuManager menuManager, Player owner) 
+	{
+		super(core, menuManager, owner, true);
+		
+		this.editorManager = menuManager;
+		this.targetHat = menuManager.getBaseHat();
+		this.totalPages = 1;
+		
+		setMenu(0, Bukkit.createInventory(null, 54, Message.EDITOR_TAG_OVERVIEW_MENU_TITLE.getValue()));
+		
+		build();
+	}
+
+	@Override
+	public void insertEmptyItem() {
+		setItem(0, 22, emptyItem);
+	}
+
+	@Override
+	public void removeEmptyItem() {
+		setItem(0, 22, null);
+	}
+
+	@Override
+	protected void build() 
+	{
+		setButton(0, 46, backButtonItem, backButtonAction);
+		setItem(0, 49, ItemUtil.createItem(CompatibleMaterial.REDSTONE_TORCH, Message.EDITOR_TAG_OVERVIEW_MENU_INFO_TITLE, Message.EDITOR_TAG_OVERVIEW_MENU_INFO));
+
+		// Add Tag
+		ItemStack addItem = ItemUtil.createItem(CompatibleMaterial.TURTLE_HELMET, Message.EDITOR_TAG_OVERVIEW_MENU_ADD_TAG);
+		setButton(0, 52, addItem, (event, slot) ->
+		{
+			EditorTagMenu editorTagMenu = new EditorTagMenu(core, editorManager, owner, (tagName) ->
+			{
+				if (tagName == null) {
+					return;
+				}
+				
+				ParticleTag tag = (ParticleTag)tagName;
+				List<ParticleTag> tags = targetHat.getTags();
+				
+				if (tags.contains(tag)) 
+				{
+					menuManager.closeCurrentMenu();
+					return;
+				}
+				
+				int size = tags.size();
+				if (size < 28)
+				{
+					ItemStack tagItem = ItemUtil.createItem(CompatibleMaterial.MUSHROOM_STEW, tagTitle.replace("{1}", tag.getDisplayName()), Message.EDITOR_TAG_OVERVIEW_MENU_TAG_DESCRIPTION);
+					setItem(0, getNormalIndex(size, 10, 2), tagItem);
+					
+					tags.add(tag);
+				}
+				
+				if (isEmpty) {
+					setEmpty(false);
+				}
+				
+				isModified = true;
+				menuManager.closeCurrentMenu();
+			});
+			
+			menuManager.addMenu(editorTagMenu);
+			editorTagMenu.open();
+			return MenuClickResult.NEUTRAL;
+		});
+		
+		// Edit Action
+		final MenuAction editAction = (event, slot) ->
+		{
+			if (event.isShiftRightClick())
+			{
+				deleteSlot(0, slot);
+				return MenuClickResult.NEGATIVE;
+			}
+			return MenuClickResult.NONE;
+		};
+		
+		for (int i = 0; i < 28; i++) {
+			setAction(getNormalIndex(i, 10, 2), editAction);
+		}
+		
+		// Tags
+		List<ParticleTag> tags = targetHat.getTags();
+		
+		if (tags.isEmpty()) 
+		{
+			setEmpty(true);
+			return;
+		}
+		
+		for (int i = 0; i < tags.size(); i++)
+		{
+			ParticleTag tag = tags.get(i);
+			ItemStack tagItem = ItemUtil.createItem(CompatibleMaterial.MUSHROOM_STEW, tagTitle.replace("{1}", tag.getDisplayName()), Message.EDITOR_TAG_OVERVIEW_MENU_TAG_DESCRIPTION);
+			
+			setItem(0, getNormalIndex(i, 10, 2), tagItem);
+		}
+	}
+
+	@Override
+	public void onClose(boolean forced) 
+	{
+		if (isModified) {
+			core.getDatabase().saveMetaData(editorManager.getMenuName(), targetHat, DataType.TAGS, -1);
+		}
+	}
+
+	@Override
+	public void onTick(int ticks) {}
+	
+	@Override
+	public void deleteSlot (int page, int slot)
+	{
+		super.deleteSlot(page, slot);
+		
+		int clampedIndex = getClampedIndex(slot, 10, 2);
+		List<ParticleTag> tags = targetHat.getTags();
+		
+		tags.remove(clampedIndex);
+		
+		if (tags.isEmpty()) {
+			setEmpty(true);
+		}
+		
+		isModified = true;
+	}
+
+}
